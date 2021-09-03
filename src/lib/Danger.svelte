13229535f1cdef49885accfd047cad83514a444e
James Bradbury <jamesbradbury93@gmail.com>
2021-09-03 09:47:43 +0100

    danger/warning components

---
+++ src/lib/Danger.svelte
@@ -0,0 +1,14 @@
+
+<div id='container'>
+	<slot/>
+</div>	
+
+<style>
+
+#container {
+	border-left: 3px solid #cc3300;
+	padding-left: 7px;
+	gap: 3px;
+}
+
+</style>
---
+++ src/lib/Warn.svelte
@@ -0,0 +1,14 @@
+
+<div id='container'>
+	<slot/>
+</div>	
+
+<style>
+
+#container {
+	border-left: 3px solid #ffcc00;
+	padding-left: 7px;
+	gap: 3px;
+}
+
+</style>
