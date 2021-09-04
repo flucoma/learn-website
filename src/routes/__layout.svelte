607b7d2346dba30bd4f917fab3f6781229cb64c7
James Bradbury <jamesbradbury93@gmail.com>
2021-09-05 00:48:47 +0100

    wangle layout a bit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,9 @@
 <script>
     import "../app.css"
+    import Header from '$lib/Header.svelte'
 </script>
+
+<Header />
 
 <div class='container'>
     <div class='navigation'></div>
@@ -13,76 +16,48 @@
 </div>
 
 <style>
-.container {
-    display: grid;
-    justify-content: center;
-}
-
-.main {
-    grid-area: main;
-    min-width: 1px;
-}
-
-.navigation {
-    grid-area: navigation;
-    width: 20ch;
-    height: max-content;
-}
-
-.empty-right {
-    grid-area: empty-right;
-    width: 20ch;
-}
-
-/* Media Queries */
-@media (min-width: 1200px) {
     .container {
-        grid-template-columns: auto min(65ch, 100%) auto;
-        grid-template-areas:
-        "navigation main empty-right"
+        display: grid;
+        justify-content: center;
+        margin: 1em;
     }
-}
-@media (max-width: 1200px) {
-    .container {
-        justify-content:start;
-        gap: 0px;
-        grid-template-rows: auto auto;
-        grid-template-areas:
-        "navigation"
-        "main"
+
+    .main {
+        grid-area: main;
+        min-width: 1px;
+    }
+
+    .navigation {
+        grid-area: navigation;
+        width: 20ch;
+        height: max-content;
     }
 
     .empty-right {
-        display: none;
+        grid-area: empty-right;
+        width: 20ch;
     }
-}
 
-#title {
-    font-size: 24px;
-    text-decoration: none;
-    padding-top: 3px;
-    padding-bottom: 3px;
-    white-space: nowrap;
-}
+    /* Media Queries */
+    @media (min-width: 1200px) {
+        .container {
+            grid-template-columns: auto min(65ch, 100%) auto;
+            grid-template-areas:
+            "navigation main empty-right"
+        }
+    }
+    @media (max-width: 1200px) {
+        .container {
+            justify-content:start;
+            gap: 0px;
+            grid-template-rows: auto auto;
+            grid-template-areas:
+            "navigation"
+            "main"
+        }
 
-#title:hover {
-    text-decoration: underline;
-    background-color: white;
-    color: black;
-}
-
-#subtitle {
-    color: grey;
-}
-
-#header {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-}
-
-#project-divider {
-    font-weight: bold;
-    text-decoration: underline;
-}
+        .empty-right {
+            display: none;
+        }
+    }
 </style>
