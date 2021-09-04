d037a38777a3b2ce135f3fdfc6c9271331e8ae6a
James Bradbury <jamesbradbury93@gmail.com>
2021-09-05 00:48:11 +0100

    styling

---
+++ src/lib/Header.svelte
@@ -0,0 +1,32 @@
+<div class='container'>
+    <a class='nav-link' href='/'>Home</a>
+    <a class='nav-link' href='/overviews'>Overviews</a>
+    <a class='nav-link' href='/about'>About</a>
+
+</div>
+
+<style>
+    .container {
+        background: #1ca3fc;
+        height: 3em;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        padding-left: 1em;
+        padding-right: 1em;
+    }
+
+    .nav-link {
+        text-decoration: none;
+        padding-right: 1em;
+        padding-left: 1em;
+    }
+
+    .nav-link:hover {
+        background: #0372b5;
+    }
+
+    .nav-link:visited {
+        color: white;
+    }
+</style>
