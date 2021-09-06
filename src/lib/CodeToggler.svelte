3f2f95143ef52c2544aca97b8ae3ef17da535ba3
James Bradbury <jamesbradbury93@gmail.com>
2021-09-06 15:10:11 +0100

    code toggler component

---
+++ src/lib/CodeToggler.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div class='container'>
+    <div>
+
+    </div>
+    <div on:click={active='max'}>Max</div>
+    <div on:click={active='sc'}>SuperCollider</div>
+    <div on:click={active='pd'}>PureData</div>
+</div>
