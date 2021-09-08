9f4d5d4535618fc0d9b133ec4aced954900cbf86
James Bradbury <jamesbradbury93@gmail.com>
2021-09-08 22:35:42 +0100

    get page structures out too

--- scripts/extract_structure.py
+++ scripts/extract_structure.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import json
 import frontmatter
+from md import parse_headings
+from util import url_from_route
 
 """
 This script will parse the routes directory of this website and construct a json with all the links.
@@ -8,7 +10,7 @@ This can be consumed in the client side javascript in order to create a site-map
 """
 
 structure = { 'routes' : {} }
-metadata = { 'data' : [] }
+page_structures = { 'pages' : {} }
 
 route_path = Path('src/routes')
 
@@ -18,32 +20,34 @@ routes = [
     if not str(x.stem).startswith('__') 
     and not x.is_dir()
     and not x.stem == 'index'
+    and x.suffix == '.svx'
 ]
 
 for route in routes:
-    if route.suffix == '.svx':
-        print(route)
-        url = Path('/'.join(list(route.parts[2:]))).with_suffix('')
-        route_data = {}
-        # Insert Section if it doesn't already exist
-        section = route.parts[2]
-        if not section in structure['routes']:
-            structure['routes'][section] = []
+    url = Path('/'.join(list(route.parts[2:]))).with_suffix('')
+    route_data = {}
 
-        route_data = { 'url' : str(url) }
-        # Now get the frontmatter and parse it
-        post = frontmatter.load(route)
-        front = post.metadata
-        if front:
-            route_data['data'] = front
-        structure['routes'][section].append(route_data)
+    # Insert Section if it doesn't already exist
+    section = route.parts[2]
+    if not section in structure['routes']:
+        structure['routes'][section] = []
+
+    route_data = { 'url' : str(url) }
+
+    url = url_from_route(route)
+
+    page_structures['pages'][url] = parse_headings(route)
+
+    # Now get the frontmatter and parse it
+    front = frontmatter.load(route).metadata
+    if front:
+        route_data['data'] = front
+    structure['routes'][section].append(route_data)
 
 
-
-
-
+# Now write some JSON to static
 with open('static/structure.json', 'w') as f:
     json.dump(structure, f, indent=2)
 
-with open('static/metadata.json', 'w') as f:
-    json.dump(metadata, f, indent=2)
+with open('static/pages.json', 'w') as f:
+    json.dump(page_structures, f, indent=2)
