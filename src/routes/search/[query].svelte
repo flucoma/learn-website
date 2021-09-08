9253eb37526dd510755e806523f3120b4178d9a0
James Bradbury <jamesbradbury93@gmail.com>
2021-09-08 18:35:52 +0100

    add a query slug for searching

---
+++ src/routes/search/[query].svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	export async function load({ page }) {
+        return { 
+            props: {
+                query: page.params.query
+            }
+        };
+	}
+</script>
+
+<script>
+    export let query;
+</script>
+
+<div>
+    This is where you would do some search with the query:
+    { query }
+</div>
