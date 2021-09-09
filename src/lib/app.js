66f581b62d5f03a49b359e2f606b98441e985b3a
James Bradbury <jamesbradbury93@gmail.com>
2021-09-09 11:49:37 +0100

    store the tag db in the app.js readable

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,4 +1,6 @@
 import { readable } from 'svelte/store';
 import s from '../../static/structure.json'
+import t from '../../static/tag.json'
 
 export const structure = readable(s);
+export const tags = readable(t);
