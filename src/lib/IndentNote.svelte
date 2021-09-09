643e4b97f188fcde1aa20761f1c199d6bc6c04db
James Bradbury <jamesbradbury93@gmail.com>
2021-09-09 13:30:48 +0100

    update the indented note component to be more flexible

---
+++ src/lib/IndentNote.svelte
@@ -0,0 +1,34 @@
+
+<script>
+	export let type= 'note';
+</script>
+
+<div id='container'
+class:note={type==='note'}
+class:warn={type==='warn'}
+class:danger={type==='danger'}
+
+>
+	<slot/>
+</div>	
+
+<style>
+
+#container {
+	padding-left: 7px;
+	gap: 3px;
+}
+
+.note {
+	border-left: 3px solid rgb(52, 52, 171);
+}	
+
+.warn {
+	border-left: 3px solid #ffcc00;
+}
+
+.danger {
+	border-left: 3px solid #cc3300;
+}
+
+</style>
