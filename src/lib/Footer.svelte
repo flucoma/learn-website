9ad00a6c40db05b63c9f8c76529c1c0a95b35140
James Bradbury <jamesbradbury93@gmail.com>
2021-09-09 10:07:38 +0100

    add a footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,77 @@
+<footer>
+    <div class='container'>
+        <div class="column">
+            <div class='address'>
+                <div class='head'>Address</div>
+                <div>FluCoMa Project</div>
+                <div>Department of Music</div>
+                <div>University of Huddersfield</div>
+                <div>Queensgate Campus</div>
+                <div>Huddersfield, West Yorkshire</div>
+                <div>England HD13DH</div>
+            </div>
+        </div>
+        <div class="column middle">
+            <div class='funding'>
+                This project has received funding from the European Research Council (ERC) under the European Union’s Horizon 2020 research and innovation programme (grant agreement No 725899). 
+            </div>
+        </div>
+        <div class="column">
+            <div class='contact'>
+                <div class="head">PHONE</div>
+                <div>+44 (0)1484 47 36 08</div>
+                
+                <div class="head">EMAIL</div>
+                <a href='mailto:info@flucoma.org'>info@flucoma.org</a>
+                
+                <div class="head">TWITTER</div>
+                <a target='_blank' href='https://twitter.com/flucoma'>twitter.com/flucoma</a>
+                
+                <div class="head">FACEBOOK</div>
+                <a target='_blank' href='https://www.facebook.com/flucoma'>facebook.com/flucoma</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<style>
+    footer {
+        width: 100%;
+        margin-top: auto;
+    }
+    .container {
+        display: grid;
+        grid-template-columns: auto auto auto;
+        justify-content: space-around;
+        background-color: rgb(8,60,100);
+        color: white;
+        font-size: 0.8em;
+    }
+
+    .column {
+        display: flex;
+        flex-direction: column;
+        margin: 1em;
+        gap: 0.75em;
+    }
+
+    .middle {
+        max-width: 300px;
+    }
+    .head {
+        font-weight: bold;
+    }
+
+    a {
+        color: white;
+    }
+
+    a:hover {
+        font-style: italic;
+    }
+
+    @media (min-width: 1200px) {
+        .container {
+        }
+    }
+</style>
