9bb82817a1397a4e545f2b5367b5de0982c0735d
James Bradbury <me@jamesbradbury.net>
2021-10-21 18:26:16 +0100

    add a numpy parsing example

---
+++ src/routes/npy-parsing.svelte
@@ -0,0 +1,21 @@
+
+<script>
+    import { fromArrayBuffer } from 'numpy-parser';
+    import { onMount } from 'svelte';
+    import { browser } from '$app/env';
+
+    let url = '/foo.npy';
+    let d = {}
+    if (browser) {
+
+        fetch(url)
+            .then(response => response.arrayBuffer())
+            .then(arr => { 
+                d = fromArrayBuffer(arr)
+                console.log(d)
+            })
+    }
+</script>
+
+
+{ d.shape }
