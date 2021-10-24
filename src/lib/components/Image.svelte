6bb27d665a22eb30b5d4b82d7542bb3f94de336b
James Bradbury <me@jamesbradbury.net>
2021-10-25 00:31:28 +0100

    add component for showing full bleed images

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let src = '';
+    export let alt = '';
+</script>
+
+<div class='container'>
+    <img 
+    alt={alt} 
+    src={src} 
+    />
+    <p>{alt}</p>
+</div>
+
+<style lang='scss'>
+
+    .container {
+        display: grid;
+        place-items: center;
+    }
+
+    .container > img {
+        padding-top: 1em;
+        padding-bottom: 1em;
+    }
+
+
+    @media (min-width: $breakpoint) {
+        .container > img {
+            width: 130%;
+        }
+    }
+
+    @media (max-width: $breakpoint) {
+        .container > img {
+            width: 100%;
+        }
+    }
+</style>
