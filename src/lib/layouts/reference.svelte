749a22fce9308ca78f5cdd048cb3e889f1ca2c87
James Bradbury <me@jamesbradbury.net>
2021-10-25 17:07:59 +0100

    add the template for reference listing

---
+++ src/lib/layouts/reference.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let title;
+    export let blurb;
+</script>
+
+<div class='title'>
+    <h1>{ title }</h1> 
+    <p class='blurb'>{ blurb }</p>
+</div>
+
+
+<slot />
+
+<h1>See Also</h1>
+
+<style lang='scss'>
+    .blurb {
+        color: grey;
+        text-align: left;
+    }
+
+    .title {
+        h1 {
+            margin-bottom: 0;
+        }
+
+        p {
+            margin-top: 0;
+        }
+    }
+
+
+</style>
