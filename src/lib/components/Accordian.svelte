1e9b6a78117409d5ecf5126da219b9afefec8302
James Bradbury <me@jamesbradbury.net>
2021-10-28 17:04:57 +0100

    base accordian component

---
+++ src/lib/components/Accordian.svelte
@@ -0,0 +1,87 @@
+<script>
+	import { slide } from 'svelte/transition';
+	export let id = '';
+    export let caption = '';
+	export let expanded = false; 
+
+	function clickHandler(e) {
+		expanded = !expanded
+	}
+</script>
+
+
+<div class='container' id={id}>
+	<button id='top-bar' on:click={ clickHandler }>
+		<div id='caption'>{caption}</div>
+		<div id='top-cap'>
+            {#if expanded}-{:else}+{/if}
+		</div>
+	</button>
+
+	{#if expanded}
+    <div id='content' 
+    in:slide={ {duration: 200 } }
+    out:slide={ {duration: 200 } }
+    >	
+        <slot/>
+    </div>
+	{/if}
+</div>	
+
+<style lang='scss'>
+
+#top-cap {
+	width: 15px;
+	margin-top: 9px;
+	margin-left: 5px;
+	margin-right: 5px;
+}
+#figure {
+	font-style: italic;
+	min-width: 80px;
+	white-space: nowrap;
+}
+
+#caption {
+	text-align: left;
+	margin-left: 1px;
+}
+
+#caption, #figure {
+	margin-top: 8px;
+	margin-bottom: 8px;
+}
+
+#top-cap {
+	display: flex;
+	flex-direction: row;
+}
+
+.container {
+	display: flex;
+	flex-direction: column;
+}
+
+#top-bar {
+    font-family: $font;
+	display: flex;
+	font-size: 15px;
+	min-height: 40px;
+	flex-direction: row;
+	justify-content: space-between;
+	border: 1px solid #f7f7f7;
+	background: #ededed;
+	box-shadow: none;
+	border-radius: 0px;
+	margin: 0px;
+}
+
+#top-bar:hover {
+	background-color: #dfdfdf;
+}
+
+#top-bar:active {
+	background-color: #cecece;
+	color: black;
+}
+</style>
