8dd8f8e1b74318987eb0810c193b50fcdf93f9e5
James Bradbury <me@jamesbradbury.net>
2021-10-28 12:26:33 +0100

    add max installation instrutions

---
+++ src/routes/installation/index.svelte
@@ -0,0 +1,12 @@
+<div class='installations'>
+    <a href='/installation/max'>Max</a>
+    <a href='/installation/max'>SuperCollider</a>
+    <a href='/installation/max'>PureData</a>
+</div>
+
+<style>
+    .installations {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
