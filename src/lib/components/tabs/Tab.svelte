40baf5f1d97f9aca306ad3121b0124be1bca4139
James Bradbury <me@jamesbradbury.net>
2021-10-28 12:26:44 +0100

    add a tab component set for doing tabbed views

---
+++ src/lib/components/tabs/Tab.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { getContext } from 'svelte';
+	import { TABS } from './Tabs.svelte';
+
+	const tab = {};
+	const { registerTab, selectTab, selectedTab } = getContext(TABS);
+
+	registerTab(tab);
+</script>
+
+<style lang='scss'>
+	button {
+		background: none;
+		border: none;
+		border-bottom: 2px solid white;
+		border-radius: 0;
+		margin: 0;
+		color: #ccc;
+        font-family: $font;
+        font-size: 1em;
+
+	}
+	
+	.selected {
+		border-bottom: 2px solid $dark-blue;
+		color: #333;
+	}
+</style>
+
+<button class:selected="{$selectedTab === tab}" on:click="{() => selectTab(tab)}">
+	<slot></slot>
+</button>
---
+++ src/lib/components/tabs/TabList.svelte
@@ -0,0 +1,9 @@
+<div class="tab-list">
+	<slot></slot>
+</div>
+
+<style lang='scss'>
+	.tab-list {
+		border-bottom: 1px solid $dark-blue;
+	}
+</style>
