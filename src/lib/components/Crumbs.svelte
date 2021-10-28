edd2b5739e78e45c44ea22b9e36aac89009b8824
James Bradbury <me@jamesbradbury.net>
2021-10-28 16:29:45 +0100

    improve breadcrumbs

--- src/lib/components/Crumbs.svelte
+++ src/lib/components/Crumbs.svelte
@@ -1,31 +1,50 @@
 <script>
     import { page } from '$app/stores';
 
-    function formatCrumb(string) {
-        string = string.charAt(0).toUpperCase() + string.slice(1);
-        return string
+    // Sanitise some special links where need be
+    const lookup = {
+        'made-with-flucoma' : 'Made with FluCoMa',
+        'reference' : 'Reference',
+        'guides' : 'Guides',
+        'overviews' : 'Overviews'
+    }
+
+    function formatCrumb(crumb) {
+        if (crumb in lookup)
+            return lookup[crumb]
+        return crumb
+        // string = string.charAt(0).toUpperCase() + string.slice(1);
+        // return string
+    }
+
+    function splitPath(path) {
+        // Splits a full page path into an array of parts
+        // This can be used to then construct the breadcrumbs
+        let d = [];
+        path = path.split('/').slice(1)
+        let accum = ''
+        path.forEach(p => {
+            accum += '/'
+            accum += p
+
+            let crumb = { url: accum, text: formatCrumb(p)}
+
+            d.push(crumb)
+        })
+        return d;
     }
     let crumbs;
 
-    // Sanitise some special links where need be
-    let linkLookup = {
-        'Made-with-flucoma' : 'Made with FluCoMa'
-    }
-    $: {
-        crumbs = $page.path.split('/').slice(1);
-        crumbs = crumbs.filter(c => c !== '');
-        crumbs = crumbs.map(c => formatCrumb(c));
-        crumbs = crumbs.map(c => {
-            return linkLookup[c] || c
-        })
-    }
+
+
+    $: crumbs = splitPath($page.path);
 </script>
 
 <!-- Breadcrumbs -->
 <div class="crumbs">
     <a href='/'>Home</a>
     {#each crumbs as c}
-        / <a href=''>{c}</a> &nbsp
+        / <a href={c.url}>{c.text}</a> &nbsp
     {/each}
 </div>
 
