24091024ee3b4037294270770971cd4d5741fd8c
James Bradbury <me@jamesbradbury.net>
2021-11-19 13:48:13 +0000

    kdtree reference

--- src/lib/widget/KDTree.svelte
+++ src/lib/widget/KDTree.svelte
@@ -5,24 +5,17 @@
 
  <script>
     import { onMount } from 'svelte';
-    import { CanvasSpace, Pt, Group } from 'pts';
-    import * as Tone from 'tone';
-    import _ from 'lodash';
-
-    // Get Data
-    import raw from '../../../static/scaling/raw.json';
-    import norm from '../../../static/scaling/norm.json';
-    import slices from '../../../static/scaling/slices.json';
+    import { CanvasSpace, Create } from 'pts';
+    import Slider from '$lib/components/Slider.svelte';
+    import Button from '$lib/components/Button.svelte';
 
     let canvas;
-    let ready = false;
-    let canPlay = false;
     let pts = [];
-    let mouse;
+    let data = [];
+    let mouse = [];
     let rect;
-    let numMatches = 2;
-    let slice;
-    let data;
+    let numNeighbours = 1;
+    let fit = false;
 
     function getMousePos(canvas, evt) {
         // We need to do this manually otherwise when shifting the window the resize is not accounted for.
@@ -33,73 +26,63 @@
         ]
     }
 
-    onMount(async() => {
-
-        // Load Sample
-        const sample = new Tone.Player('/scaling/src.wav', () => { console.log(ready = true) }).toDestination();
-        sample.loop = 1;
-        
-        let space = new CanvasSpace('#sketch').setup({ 
+    onMount(async() => {      
+        let space = new CanvasSpace('#sketch')
+        space.setup({ 
             bgcolor: '#e8e4ec',
-            autoResize: true 
+            resize: true
         });
+
         let form = space.getForm();
         rect = canvas.getBoundingClientRect();
         space.add({
             start: () => {
-                data = _.mapValues(norm.data, pt => new Pt([ pt[0]*space.width, pt[1]*space.height ]));
-                pts = Group.fromArray(Object.values(data));
+                // Generate a random space of two-dimensional points between 0.0 and 1.0;
+                pts = Create.distributeRandom( space.innerBound, 120 );
             },
             animate: (time, ftime, space) => {
-                _.forEach(data, (v, k) => { 
-                    data[k]['dist'] = v.$subtract(mouse).magnitude()
-                })
+                form.fillOnly("#123").points( pts, 3, "circle" );
+                if (fit) {
+                    pts.sort((a, b) => a.$subtract(mouse).magnitude() - b.$subtract(mouse).magnitude())
 
-                let distances = _.map(data, v => v.dist);
-                slice = Object.keys(data)[_.indexOf(distances, _.min(distances))]
+                    // Draw lines from mouse to points
+                    for (let j=0; j < numNeighbours; j++) {
+                        form.strokeOnly('#0d47a1', 2).line( [ pts[j], mouse ] );
+                    }
 
-                let start = slices.data[slice][0]
-                let end = start + slices.data[slice][1]
-
-                sample.loopStart = start / 44100.0
-                sample.loopEnd = end / 44100.0
-
-                pts.sort((a, b) => a.$subtract(mouse).magnitude() - b.$subtract(mouse).magnitude())
-
-                for (let i=1; i < numMatches; i++) {
-                    form.fillOnly("#123").points( pts.slice(0, numMatches), 3, "circle" );
-                }
-                for (let j=0; j < numMatches; j++) {
-                    form.strokeOnly('#0d47a1', 2).line( [ pts[j], pts[0] ] );
-                }
-                form.fill("#f03").point( pts[0], 10, "circle" );
-                form.fillOnly("#123").points( pts.slice(numMatches), 2, "circle" );
+                    // Draw bigger circles on top of points
+                    for (let i=0; i < numNeighbours; i++) {
+                        form.fill("#f03").point( pts[i], 7, "square" );
+                    }
+                };
             },
             action: (t,x,y,e) => {
-                if (e.type === 'mousedown') {
-                    sample.start()
-                } else if (e.type === 'mouseup') {
-                    sample.stop()
-                }
-
                 mouse = getMousePos(canvas, e)
             }
         });
-        space.play().bindMouse();
-        ready = true;
+        space.bindMouse().bindTouch().play();
     })
 </script>
 
-{ canPlay }
+<div class="container">
+    <canvas id='sketch' bind:this={ canvas } />
+</div>
 
-{ slice }
-
-<canvas id='sketch' bind:this={ canvas } />
+<div class="controls">
+    <Button on:click={ () => fit = true } label='Fit' />
+    <Slider bind:value={numNeighbours} min={1} max={15} title='Number of Neighbours' step={1} />
+</div>
 
 <style>
     #sketch {
         width: 100%;
         display: inline-block;
+    }
+
+    .controls {
+        display: flex;
+        flex-direction: row;
+        gap: 1em;
     }
 </style>
 
