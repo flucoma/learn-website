ecdeff348a6e397c974b5e863a3d2c8e9f6d753b
Jacob <jacob.dchart@gmail.com>
2021-11-23 16:27:50 +0000

    Audio Player Component

---
+++ src/lib/components/SimpleAudioPlayer.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let src = '';
+</script>
+
+<div class='container'>
+    <audio
+        controls
+        src={src}>
+    </audio>
+</div>
+
+<style>
+    .container {
+        padding-top: 1em;
+        padding-bottom: 1em;
+    }
+    
+    audio {
+        max-width: 100%;
+        margin: 0 auto;
+        display: block;
+    }
+</style>
