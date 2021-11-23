cc56816a00c387fce12c16e6922536218e638f4b
Jacob <jacob.dchart@gmail.com>
2021-11-23 16:28:09 +0000

    Big Quote Component

---
+++ src/lib/components/BigQuote.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let txt = '';
+</script>
+
+<div class='container'>
+    {txt}
+</div>
+
+<style>
+    .container {
+        padding-top: 1em;
+        padding-bottom: 1em;
+
+        margin-left: 1em;
+        margin-right: 1em;
+    }
+</style>
