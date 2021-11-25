ceed5a4ce488cf2d3ee1d1d452d420e9d3cbd74c
James Bradbury <me@jamesbradbury.net>
2021-11-25 15:39:32 +0000

    make image prettier

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -2,48 +2,45 @@
     export let src = '';
     export let alt = '';
     export let label = '';
-    export let drawBorder = true;
 </script>
 
-<a target='_blank' href={src}>
-<div class='container' class:border={drawBorder}>
+<div class="container">
+    <a target='_blank' href={src}>
         <img 
         alt={alt} 
         src={src}
         />
+    </a>
+    <div class='label'>
+        {@html label}
     </div>
-</a>
-<p class='label'>
-    {@html label}
-</p>
+</div>
 
 <style lang='scss'>
-
     .container {
         display: grid;
         place-items: center;
         width: 80%;
         margin: 0 auto;
-    }
 
-    .border {
-        border-radius: 2px;
-        box-shadow: 0 1px 3px 4px rgba(0, 0, 0, 0.1);
-    }
+        img {
+            padding-top: 1em;
+            padding-bottom: 1em;
+            max-width: 100%;
+        }
 
-    a {
-        max-width: 100%;
-    }
+        a {
+            max-width: 100%;
+        }
 
-    .container > img {
-        padding-top: 1em;
-        padding-bottom: 1em;
-        max-width: 100%;
+        a:hover {
+            background:transparent;
+        }
     }
 
     .label{
         text-align: center;
-        font-size: small;
+        font-size: 0.8rem;
         font-style: italic;
     }
 </style>
