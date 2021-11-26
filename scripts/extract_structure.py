e43384140f094f2fbbb5ebde28c6e84e53f54728
James Bradbury <me@jamesbradbury.net>
2021-11-26 21:51:21 +0000

    use git cli to get edit times of all files

--- scripts/extract_structure.py
+++ scripts/extract_structure.py
@@ -1,6 +1,7 @@
-from pathlib import Path
 import json
 import frontmatter
+from subprocess import getstatusoutput as cli
+from pathlib import Path
 from md import parse_headings
 from util import url_from_route
 
@@ -12,6 +13,7 @@ This can be consumed in the client side javascript in order to create a site-map
 structure = { 'routes' : {} }
 page_structures = { 'pages' : {} }
 tag_db = { 'db' : {} }
+last_edits = { 'routes' : {} }
 
 route_path = Path('src/routes')
 
@@ -25,6 +27,26 @@ routes = [
 ]
 
 for route in routes:
+    status, cli_output = cli(f'git log -1 {str(route)}')
+    # Im horrible and im sorry 🚔 crime
+    output_split = cli_output.split('\n')
+    commit = output_split[0]
+    author = output_split[1]
+    modtime = output_split[2]
+    
+    last_commit = commit.split(' ')[1].lstrip().rstrip()
+    author_name = author.split(':')[1].split('<')[0].lstrip().rstrip()
+    last_edited = modtime.split(':', 1)[1].split('+')[0].lstrip().rstrip()
+    last_edited = last_edited.split(' ')[1:4]
+    last_edited = ' '.join(last_edited)
+
+    last_edits['routes'][url_from_route(route)] = {
+        'commit' : last_commit,
+        'author' : author_name,
+        'time' : last_edited,
+        'url' : str(route)
+    }
+
     url = Path('/'.join(list(route.parts[2:]))).with_suffix('')
     route_data = {}
 
@@ -61,3 +83,6 @@ with open('static/structure.json', 'w') as f:
 
 with open('static/pages.json', 'w') as f:
     json.dump(page_structures, f, indent=2)
+
+with open('static/edits.json', 'w') as f:
+    json.dump(last_edits, f, separators=(',', ':'))
