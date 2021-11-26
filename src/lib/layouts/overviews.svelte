3023614bfb55eedf254fc10d3a35989717302a1b
James Bradbury <me@jamesbradbury.net>
2021-11-26 11:35:09 +0000

    update overview format

--- src/lib/layouts/overviews.svelte
+++ src/lib/layouts/overviews.svelte
@@ -1,59 +1,61 @@
 <script>
-	export let title;
-	export let blurb;
-	export let tags;
+    export let title;
+    export let blurb;
+    export let tags;
 </script>
 
-<div class="title-box">
-	<h1>{title}</h1>
-	<p class="blurb">{blurb}</p>
-</div>
+<div class='title-box'>
+    <h1>{ title }</h1>  
+    <p class='blurb'>
+        { blurb }
+    </p>
 
-<h2>Tags</h2>
-<div class="tagtainer">
-	{#each tags as tag}
-		<a class="tag" href="/search/{tag}">
-			{tag}
-		</a>
-	{/each}
+    <div class='tagtainer'>
+        {#each tags as tag}
+        <a class='tag' href='/search/{tag}'>
+            { tag }
+        </a>
+        {/each}
+    </div>
 </div>
 
 <slot />
 
-<style lang="scss">
-	h1 {
-		text-align: center;
-	}
-	.blurb {
-		color: grey;
-		text-align: center;
-	}
-	.tagtainer {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		gap: 1em;
-	}
+<style lang='scss'>
 
-	.tag {
-		width: max-content;
-		padding: 0.15em;
-		border-radius: 0px;
-		box-shadow: 0 1px 1.5px 2px rgba(0, 0, 0, 0.1);
-		background-color: transparent;
-		text-align: center;
-	}
+    :global(p) {
+        text-align: justify;
+        text-justify: inter-word;
+    }
+    .blurb {
+        color: grey;
+    }
+    .tagtainer {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 1em;
+    }
 
-	.tag:hover {
-		color: white;
-		text-decoration: underline;
-	}
+    .tag {
+        width: max-content;
+        padding: 0.15em;
+        border-radius: 0px;
+        box-shadow: 0 1px 1.5px 2px rgba(0, 0, 0, 0.1);
+        background-color: transparent;
+        text-align: center;
+    }
 
-	.title > h1 {
-		margin-bottom: 0;
-	}
+    .tag:hover {
+        color: white;
+        text-decoration: underline;
+    }
 
-	.title > p {
-		margin-top: 0;
-	}
+    .title > h1 {
+        margin-bottom: 0;
+    }
+
+    .title > p {
+        margin-top: 0;
+    }
 </style>
