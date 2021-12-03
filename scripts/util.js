719f2fbdb9260540eab5cfcb0eb02643dd010ecf
James Bradbury <me@jamesbradbury.net>
2021-12-03 00:43:54 +0000

    fix url parsing stuff

--- scripts/util.js
+++ scripts/util.js
@@ -2,8 +2,21 @@ import path from 'path';
 
 const urlFromRoute = (route) => {
     let parse = path.parse(route);
-    let base = parse.dir.split('/').slice(1).join('/');
+    let base = parse.dir.split('/').slice(2).join('/');
     return `/${base}/${parse.name}`
 };
 
-export { urlFromRoute }
+const sanitiseHashLink = (link) => {
+    link = link.replace('.', '');
+    link = link.replace("'", '');
+    if (link.slice(0, 2) == '--') {
+        link = link.replace('--', '')
+    }
+    link = link.replace('/', '');
+    link = link.replace('(', '');
+    link = link.replace(')', '');
+    link = link.toLowerCase();
+    link = link.replace(' ', '-')
+    return link
+}
+export { urlFromRoute, sanitiseHashLink }
