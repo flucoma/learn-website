61bf65fd3816960f5a68923ec132a04ef640aa07
James Bradbury <me@jamesbradbury.net>
2021-12-08 12:06:09 +0000

    update component style for bandcamp

--- src/lib/components/BandCamp.svelte
+++ src/lib/components/BandCamp.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	export let url: string = '';
-	export let album_id: string = '';
+	export let id: string = '';
 	export let title: string = '';
 	export let caption: string = title;
 	export let width: number = 560;
@@ -8,25 +8,28 @@
 </script>
 
 <div class="container">
-	<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album={album_id}/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless>
+	<iframe
+	title={title} 
+	src="https://bandcamp.com/EmbeddedPlayer/album={id}/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" 
+	seamless
+	/>
 		
-	</iframe>
-
 	<div class="caption">
 		<p>{caption}</p>
 	</div>
 </div>
 
-<style>
+<style lang='scss'>
 	.container {
 		padding-top: 1em;
 		padding-bottom: 1em;
-	}
 
-	iframe {
-		max-width: 100%;
-		margin: 0 auto;
-		display: block;
+		iframe {
+			max-width: 100%;
+			margin: 0 auto;
+			display: block;
+			height: 120px;
+		}
 	}
 
 	.caption :global(p) {
