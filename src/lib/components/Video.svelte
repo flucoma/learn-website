9c99d73eff39c555375dedc22d457ab9e142d8d8
James Bradbury <me@jamesbradbury.net>
2022-01-04 11:27:57 +0000

    create a video component for mp4/webm

---
+++ src/lib/components/Video.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let url: string = '';
+    export let label= "";
+
+    url = url.replace(/\.[^/.]+$/, ""); // remove file extension
+</script>
+
+<video controls loop>
+    <source src={`${url}.mp4`} type="video/mp4">
+    <source src={`${url}.webm`} type="video/webm">
+    <p>Your browser doesn't support HTML5 video. </p>
+    <track kind=captions>
+</video>
+
+<div class="caption">
+    {label}
+</div>
