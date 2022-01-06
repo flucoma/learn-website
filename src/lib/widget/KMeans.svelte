a1e2f4892f6159c9275340e747d8a80f12dc41f7
James Bradbury <me@jamesbradbury.net>
2022-01-06 10:22:50 +0000

    update typescript annotations

--- src/lib/widget/KMeans.svelte
+++ src/lib/widget/KMeans.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import KMeans from 'tf-kmeans';
     import * as tf from '@tensorflow/tfjs';
     import * as d3 from 'd3';
@@ -6,7 +6,7 @@
     import { Chart, registerables } from 'chart.js';
     import Button from '$lib/components/Button.svelte';
     import Select from 'svelte-select';
-
+    
     // Data Imports TODO: fetch these on load to reduce component payload
     import gaussianTiny from '../../../static/data/gaussian4-tiny.json';
     import gaussianSmall from '../../../static/data/gaussian4-small.json';
@@ -17,36 +17,36 @@
     import blobsData from '../../../static/data/blobs.json';
     import circlesData from '../../../static/data/circles.json';
     import random from '../../../static/data/random.json';
-
+    
     // Configure some options for KMeans
-    let numClusters = 4;
+    let numClusters: number = 4;
     
     // Set aside some variables to do with the kmeans centroids
-    let predictions = [];
-    let centroids = [];
-    let iteration = 0;
+    let predictions: number[] = [];
+    let centroids: number[] = [];
+    let iteration: number = 0;
     
     // Chart.js
     let ctx, canvas, chart;
     
     // Essentially rename the colour generation function
     var genColour = d3.interpolateSinebow;
-    let kmeans;
+    let kmeans: KMeans;
     // Declare some vars to use after mounting
     let doMeans; // A function that the button gets bound to. We won't define it yet because of awaits
     
     const items = [
-        { value: 'gaussian', label: 'Four Gaussian Clusters (Large)' },
-        { value: 'gaussianSmall', label: 'Four Gaussian Clusters (Small)' },
-        { value: 'gaussianTiny', label: 'Four Gaussian Clusters (Tiny)' },
-        { value: 'line', label: 'Synthetic Line (Large)' },
-        { value: 'lineTiny', label: 'Synthetic Line (Tiny)' },
-        { value: 'moon', label: 'Moons (Small)' },
-        { value: 'blobs', label: 'Blobs (Small)' },
-        { value: 'circles', label: 'Circles (Small)' },
-        { value: 'random', label: 'Random (Small)' },
+    { value: 'gaussian', label: 'Four Gaussian Clusters (Large)' },
+    { value: 'gaussianSmall', label: 'Four Gaussian Clusters (Small)' },
+    { value: 'gaussianTiny', label: 'Four Gaussian Clusters (Tiny)' },
+    { value: 'line', label: 'Synthetic Line (Large)' },
+    { value: 'lineTiny', label: 'Synthetic Line (Tiny)' },
+    { value: 'moon', label: 'Moons (Small)' },
+    { value: 'blobs', label: 'Blobs (Small)' },
+    { value: 'circles', label: 'Circles (Small)' },
+    { value: 'random', label: 'Random (Small)' },
     ];
-
+    
     let dataLookup = {
         'gaussian' : gaussianData,
         'gaussianTiny' : gaussianTiny,
@@ -58,7 +58,7 @@
         'circles' : circlesData,
         'random' : random,
     };
-
+    
     let data = Object.values(gaussianSmall.data);
     
     const formatForChart = (d) => {
@@ -104,32 +104,32 @@
             k: numClusters,
             maxIter: 40,
         });
-
+        
         kmeans.TrainAsync(tfData,
-            async(i, cent, pred) => {
-                iteration = i;
-                predictions = await pred.array();
-                centroids = await cent.array();
-                
-                // Append centroids to the dataset
-                let d = JSON.parse(JSON.stringify(data))
-                d.push(...centroids);
-
-                // Compute all the colours
-                let colours = predictions.map(p => genColour(p / centroids.length));
-                let centroidColours = centroids.map((c, i) => genColour(i / centroids.length));
-                colours.push(...centroidColours);
-                
-                // Now calculate all the point radii
-                // The last k points shold be the centroids so they need to be larger and in charger
-                let rad = d.map((p, i) => i+1 <= predictions.length ? 3 : 10);
-
-                chart.data.datasets[0].data = formatForChart(d)
-                chart.data.datasets[0].pointBackgroundColor = colours;
-                chart.data.datasets[0].pointBorderColor = colours;
-                chart.data.datasets[0].pointRadius = rad;
-                chart.update();
-            }
+        async(i, cent, pred) => {
+            iteration = i;
+            predictions = await pred.array();
+            centroids = await cent.array();
+            
+            // Append centroids to the dataset
+            let d = JSON.parse(JSON.stringify(data))
+            d.push(...centroids);
+            
+            // Compute all the colours
+            let colours = predictions.map(p => genColour(p / centroids.length));
+            let centroidColours = centroids.map((c, i) => genColour(i / centroids.length));
+            colours.push(...centroidColours);
+            
+            // Now calculate all the point radii
+            // The last k points shold be the centroids so they need to be larger and in charger
+            let rad = d.map((p, i) => i+1 <= predictions.length ? 3 : 10);
+            
+            chart.data.datasets[0].data = formatForChart(d)
+            chart.data.datasets[0].pointBackgroundColor = colours;
+            chart.data.datasets[0].pointBorderColor = colours;
+            chart.data.datasets[0].pointRadius = rad;
+            chart.update();
+        }
         );
     };
     
@@ -138,11 +138,11 @@
         iteration = 0;
         predictions = null;
         centroids = null;
-
+        
         const selection = e.detail.value;
-
+        
         data = Object.values(
-            dataLookup[selection].data
+        dataLookup[selection].data
         )
         
         chart.data.datasets[0].pointBackgroundColor = ['#000'];
@@ -161,14 +161,14 @@
     />
     
     <div class="controls">
-
+        
         <Select 
         {items} 
         on:select={updateData} 
         placeholder='Select a dataset...'
         showChevron={true}
         />
-
+        
         <div class='cluster'>
             <div class='select'>
                 <div>Number of Clusters</div>
@@ -177,7 +177,7 @@
                 type=number bind:value={numClusters} min=1 max=49
                 >
             </div>
-    
+            
             <div>Iteration Number: { iteration }</div>
         </div>
     </div>
@@ -185,7 +185,7 @@
     <canvas id="sketch" bind:this={canvas} />
 </div>
 
-<style lang='scss'>
+<style lang='postcss'>
     .container {
         display: flex;
         flex-direction: column;
@@ -196,37 +196,38 @@
         display: grid;
         grid-template-rows: auto auto;
         gap: 1em;
-
-        .cluster {
-            display: flex;
-            flex-direction: row;
-            place-items: center;
-            justify-content: space-between;
-
-            .select {
-                display: flex;
-                flex-direction: row;
-                place-items: center;
-                gap: 0.5em;
-            }
-        }
-
-        input {
-            padding: 0.3em;
-            font-size: 1rem;
-        }
-
-        /* Chrome, Safari, Edge, Opera */
-        input::-webkit-outer-spin-button,
-        input::-webkit-inner-spin-button {
+    }
+    
+    .cluster {
+        display: flex;
+        flex-direction: row;
+        place-items: center;
+        justify-content: space-between;
+    }
+    
+    
+    .select {
+        display: flex;
+        flex-direction: row;
+        place-items: center;
+        gap: 0.5em;
+    }
+    
+    input {
+        padding: 0.3em;
+        font-size: 1rem;
+    }
+    
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
         -webkit-appearance: none;
         margin: 0;
-        }
-
-        /* Firefox */
-        input[type=number] {
+    }
+    
+    /* Firefox */
+    input[type=number] {
         -moz-appearance: textfield;
-        }
     }
     #sketch {
         width: 100%;
