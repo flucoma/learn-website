9f43b94c30f24084a394f6fbfe8c0a8e6e7bc41e
James Bradbury <me@jamesbradbury.net>
2022-01-06 12:38:29 +0000

    make footer more minimal

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,93 +1,31 @@
+<script>
+	import HR from '$lib/components/HR.svelte';
+</script>
+
 <footer>
-	<div class="container">
-		<div class="column">
-			<div class="head">ADDRESS</div>
-			<div class="address">
-				<div>FluCoMa Project</div>
-				<div>Department of Music</div>
-				<div>University of Huddersfield</div>
-				<div>Queensgate Campus</div>
-				<div>Huddersfield, West Yorkshire</div>
-				<div>England HD13DH</div>
-			</div>
-		</div>
+	<HR />
+	<div class="content">
+		<a target='_blank' href='https://github.com/flucoma/learn-website/issues/new'>Problem? File an issue</a>
+		<a target='_blank' href='https://flucoma.org'>Go to FluCoMa Home Page</a>
 
-		<div class="column">
-			<div class="head">CONTACT</div>
-			<div class="contact">
-				e: <a href="mailto:info@flucoma.org">info@flucoma.org</a>
-			</div>
-
-			<div class="contact">
-				t: <a target="_blank" href="https://twitter.com/flucoma">twitter.com/flucoma</a>
-			</div>
-
-			<div class="contact">
-				f: <a target="_blank" href="https://www.facebook.com/flucoma">facebook.com/flucoma</a>
-			</div>
-
-			<div class="contact">
-				<div>p: +44 (0)1484 47 36 08</div>
-			</div>
-		</div>
-
-		<div class="column">
-			<div>© University of Huddersfield. All rights reserved.</div>
-			<div class="funding">
-				This project has received funding from the European Research Council (ERC) under the
-				European Union’s Horizon 2020 research and innovation programme (grant agreement No 725899).
-			</div>
-		</div>
 	</div>
 </footer>
 
-<style lang="postcss">
+<style>
 	footer {
-		width: 100%;
-		margin-top: auto;
-	}
-	.container {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: center;
-		background-color: var(--dark-blue);
-		color: white;
-		font-size: 0.8em;
-		gap: 2em;
-		padding-top: 2em;
+		margin: 1em;
+		text-align: right;
 	}
 
-	@media (min-width: 1200px) {
-		.container {
-			justify-content: center;
-		}
+	.content {
+		display: grid;
+		grid-template-columns: auto auto;
+		justify-content: space-around;
 	}
 
-	@media (max-width: 1200px) {
-		.container {
-			justify-content: flex-start;
-		}
+	.content > a {
+		font-size: 0.8rem;
+		text-decoration: none;
 	}
 
-	.column {
-		display: flex;
-		flex-direction: column;
-		padding-left: 2em;
-		padding-right: 2em;
-		max-width: 45ch;
-	}
-
-	.funding {
-		text-align: justify;
-		font-size: 0.75em;
-	}
-
-	.address {
-		white-space: nowrap;
-	}
-
-	.head {
-		font-weight: bold;
-	}
 </style>
