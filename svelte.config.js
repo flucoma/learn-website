9a98c7abc53e4cfbde58b9db4adfe3954c7252cc
James Bradbury <me@jamesbradbury.net>
2022-01-07 18:03:52 +0000

    append an svg to each heading

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,21 @@ const config = {
 	preprocess: [
 		preprocess(),
 		mdsvex({
-			rehypePlugins: [headings, slug],
+			rehypePlugins: [
+				slug, 
+				[
+					headings,
+					{
+						behavior: 'append',
+						test: ['h2', 'h3', 'h4', 'h5', 'h6'], // don't auto-link <h1>
+						content: h('img', { 
+							src: '/img/onlylogo.svg',
+							class: 'autolink'
+						}),
+						// <img class="logo" src="/img/onlylogo.svg" alt="FluCoMa Logo" loading="lazy" />
+					  },
+				]
+			],
 			layout: {
 				overviews: './src/lib/layouts/overviews.svelte',
 				reference: './src/lib/layouts/reference.svelte',
