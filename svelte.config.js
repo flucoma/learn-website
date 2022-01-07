49cc3f982286f7024cb9d50878cfa6a7d3f5a8b8
James Bradbury <me@jamesbradbury.net>
2022-01-07 18:03:44 +0000

    import hastscript

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import slug from 'rehype-slug';
 import headings from 'rehype-autolink-headings';
-
+import { h } from 'hastscript';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
