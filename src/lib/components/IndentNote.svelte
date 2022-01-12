394dc6e4daf8e514181feef9cd2dd65bcbd39652
James Bradbury <me@jamesbradbury.net>
2022-01-12 23:41:13 +0000

    add a pointer mode for indentnode

--- src/lib/components/IndentNote.svelte
+++ src/lib/components/IndentNote.svelte
@@ -7,34 +7,47 @@ class="container"
 class:note={type === 'note'}
 class:warn={type === 'warn'}
 class:danger={type === 'danger'}
+class:pointer={type === 'pointer'}
 >
 <slot />
 </div>
 
 <style>
 	.container {
-		--bg-color: rgb(52, 52, 171, 0.2);
-		--border-color: rgb(52, 52, 171, 1.0);
-		margin-top: 1em;
+		--bg-color: rgba(224,244,252, 1.0);
+		--border-color: rgba(52, 52, 171, 0.63);
+		margin-top: 0.5em;
+		margin-bottom: 0.5em;
 		padding-left: 0.5em;
 		padding-right: 0.5em;
 		background-color: var(--bg-color);
 		border: 3px solid var(--border-color);
 		color: rgb(65, 65, 65);
+		border-radius: 0.25rem;
+		padding: 0.5em;
+	}
+
+	.container > :global(p) {
+		margin: 0;
 	}
 	
 	.note {
-		--bg-color: rgb(52, 52, 171, 0.2);
+		--bg-color: rgba(52, 52, 171, 0.2);
 		--border-color: rgba(31, 31, 136, 0.63);
 	}
 	
 	.warn {
-		--bg-color: rgb(255, 204, 0, 0.2);
-		--border-color: rgb(255, 204, 0, 0.63);
+		--bg-color: rgba(255, 204, 0, 0.2);
+		--border-color: rgba(255, 204, 0, 0.63);
 	}
 	
 	.danger {
-		--bg-color: rgb(204, 51, 0, 0.2);
-		--border-color: rgb(204, 51, 0, 0.63);
+		--bg-color: rgba(204, 51, 0, 0.2);
+		--border-color: rgba(204, 51, 0, 0.63);
+	}
+
+	.pointer {
+		--bg-color: rgba(230, 252, 224, 1.0);
+		--border-color: rgb(133, 229, 106);
 	}
 </style>
