0977912ba50b7da41a261cee082d5b1488244454
James Bradbury <me@jamesbradbury.net>
2022-01-12 13:31:01 +0000

    fix styling and some layout properties

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,31 +1,50 @@
 <script>
 	import HR from '$lib/components/HR.svelte';
+	import ArrowRight from '$lib/components/ArrowRight.svelte';
 </script>
 
 <footer>
 	<HR />
 	<div class="content">
-		<a target='_blank' href='https://github.com/flucoma/learn-website/issues/new'>Problem? File an issue</a>
-		<a target='_blank' href='https://flucoma.org'>Go to FluCoMa Home Page</a>
-
+		<a 
+		target='_blank' 
+		href='https://github.com/flucoma/learn-website/issues/new'
+		>
+		Problem? File an issue <ArrowRight />
+		</a>
+		
+		<a 
+		target='_blank' 
+		href='https://flucoma.org'
+		>
+		Go to FluCoMa Home Page <ArrowRight />
+		</a>
 	</div>
 </footer>
 
 <style>
 	footer {
-		margin: 1em;
-		text-align: right;
+		min-height: var(--footer-height);
 	}
-
 	.content {
 		display: grid;
 		grid-template-columns: auto auto;
 		justify-content: space-around;
+		max-width: var(--max-text-width);
+		margin: 0 auto;
 	}
 
 	.content > a {
 		font-size: 0.8rem;
 		text-decoration: none;
+		display: grid;
+		grid-template-columns: auto auto;
+		place-items: center;
+	}
+
+	a:hover {
+		background-color: transparent;
+		color: var(--med-blue);
 	}
 
 </style>
