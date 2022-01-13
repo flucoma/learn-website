a834d5ef498464e1a94fd59f1e6b8deb2d3588c4
James Bradbury <me@jamesbradbury.net>
2022-01-13 15:32:27 +0000

    reimagining of the references page

---
+++ src/routes/reference/index.svelte
@@ -0,0 +1,42 @@
+<h1>Reference</h1>
+
+<p>
+	This section of the learn platform outlines each algorithm of the Fluid Corpus Manipulation toolbox. The aim of these outlines is to help you develop a fundamental understanding of the algorithms themselves while also nurturing a musical intuition for how it might be used and applied to creative problems and goals. Use the search box below to search for a specific algorithm.
+</p>
+
+<section class="section">
+	<h2>Slicers</h2>
+	<ul>
+		<li>NoveltySlice</li>
+		<li>NoveltySlice</li>
+		<li>NoveltySlice</li>
+		<li>NoveltySlice</li>
+		<li>NoveltySlice</li>
+	</ul>
+</section>
+
+
+
+
+
+
+<style>
+	.section {
+
+	}
+
+	.section ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	.section ul li {
+		display: block;
+	}
+</style>
+
+
+
+
+
+
