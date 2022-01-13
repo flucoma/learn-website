d38c3f601e00054cac0a13d243164327da111699
James Bradbury <github@jamesbradbury.net>
2022-01-13 18:27:16 +0000

    [Enhance] Update reference page searchability (#163)
    * rework the reference page search
    
    * reimagining of the references page
    
    * add missing references
    
    * add categories to references
    
    * update index.svelte
    
    * add missing references
    
    * change name of STFT
    
    * update style of references page
    
    * remove duplicate entry
    
    * add bufthresh
    
    * reorder categories
    
    * use flex for quasi-masonry layout

---
+++ src/routes/reference/index.svelte
@@ -0,0 +1,79 @@
+<script>
+	import { docs } from '$lib/app';
+	import ArrowRight from '$lib/components/ArrowRight.svelte';
+
+	let references = docs.filter(d => d.section === 'reference');
+
+	const categorised = {
+		'Analyse Data' : [],
+		'Analyse Sound' : [],
+		'Utility' : [],
+		'Decompose' : [],
+		'Slicing' : [],
+		'Transform' : []
+	}
+
+	references.forEach(r => { categorised[r.category].push(r) });
+</script>
+
+<h1>Reference</h1>
+<p>
+	This section of the learn platform outlines each algorithm of the Fluid Corpus Manipulation toolbox. The aim of these outlines is to help you develop a fundamental understanding of the algorithms themselves while also nurturing a musical intuition for how it might be used and applied to creative problems and goals. Use the search box below to search for a specific algorithm.
+</p>
+
+<div class="container">
+	{#each Object.entries(categorised) as [category, ref]}
+	<section>
+		<h3>{ category }</h3>
+		{#each ref as r}
+		<ul>
+			<li>
+				<a href={r.url}>{r.title} <ArrowRight /></a>
+			</li>
+		</ul>
+		{/each}
+	</section>
+	{/each}
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+		max-height: 550px;
+	}
+
+	.container > section {
+		width: 33%;
+	}
+
+	section > h3 {
+		margin-top: 0.25em;
+		margin-bottom: 0.25em;
+	}
+
+	section ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	section ul li {
+		display: block;
+		margin: 0;
+	}
+
+	section ul li a {
+		text-decoration: none;
+		display: grid;
+		grid-template-columns: auto auto;
+		place-items: center;
+		gap: 0.125em;
+	}
+</style>
+
+
+
+
+
+
