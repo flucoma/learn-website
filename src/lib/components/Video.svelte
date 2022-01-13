c7a490ab5adf17b996826432ff43606130b99937
James Bradbury <me@jamesbradbury.net>
2022-01-13 14:39:31 +0000

    update video component to be more user friendly

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -5,13 +5,35 @@
     url = url.replace(/\.[^/.]+$/, ""); // remove file extension
 </script>
 
-<video controls loop>
-    <source src={`${url}.mp4`} type="video/mp4">
-    <source src={`${url}.webm`} type="video/webm">
-    <p>Your browser doesn't support HTML5 video. </p>
-    <track kind=captions>
-</video>
+<figure>
+    <video controls loop>
+        <source src={`${url}.mp4`} type="video/mp4">
+        <source src={`${url}.webm`} type="video/webm">
+        <p>Your browser doesn't support HTML5 video. </p>
+        <track kind=captions>
+    </video>
 
-<div class="caption">
-    {label}
-</div>
+    <figcaption class="caption">
+        {label}
+    </figcaption>
+
+</figure>
+
+<style>
+    figure {
+        display: grid;
+        grid-template-rows: auto auto;
+        place-items: center;
+        gap: 1em;
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+    video {
+        max-width: 100%;
+    }
+
+    .caption {
+		font-size: 0.75rem;
+    }
+</style>
