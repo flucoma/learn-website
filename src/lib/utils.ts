4f165cbe57e1f3d25f9d8eb461573df3c11fa9e9
James Bradbury <me@jamesbradbury.net>
2022-01-17 18:40:06 +0000

    add typescript utilities

---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+function logslider(position, imin, imax, omin, omax) {
+    // position will be between 0 and 100
+    var minp = imin
+    var maxp = imax;
+    
+    // The result should be between 100 an 10000000
+    var minv = Math.log(omin);
+    var maxv = Math.log(omax);
+    
+    // calculate adjustment factor
+    var scale = (maxv-minv) / (maxp-minp);
+    
+    return Math.exp(minv + scale*(position-minp));
+}
+
+export { logslider }
