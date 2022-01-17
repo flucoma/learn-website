a1c6125011a017fb619f1c8ad2d7b71c467fca56
Jacob <jacob.dchart@gmail.com>
2022-01-17 12:05:15 +0000

    Static Video Player Component

---
+++ src/lib/components/Video.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let src: string = '';
+	export let label: string = '';
+</script>
+
+<video controls>
+
+    <source src={src}
+            type="video/mp4">
+
+    Sorry, your browser doesn't support embedded videos.
+</video>
+
+<figcaption class="label">
+	{label}
+</figcaption>
+
+<style lang="postcss">
+	.label {
+		text-align: center;
+		font-size: 0.8rem;
+		font-style: italic;
+	}
+</style>
