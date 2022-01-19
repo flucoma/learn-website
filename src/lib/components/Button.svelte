b74cb084d49f66f1f1cfa15c2f7fa136b06c75fa
James Bradbury <me@jamesbradbury.net>
2022-01-19 17:20:44 +0000

    add some inplace changes to button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,16 +2,23 @@
 	import { createEventDispatcher } from 'svelte';
 	export let label: string = 'button';
 	export let width: string = '100px';
+	export let height: string = '38px';
 	export let disabled: boolean = false;
 	const dispatch = createEventDispatcher();
-
+	
 	function clickHandler() {
 		dispatch('click');
 	}
 </script>
 
-<button style={`width: ${width}`} class:disabled class="btn" on:click={clickHandler} {disabled}>
-	{label}
+<button 
+style:width={width} 
+class:disabled 
+class="btn" 
+on:click={clickHandler} 
+{disabled}
+>
+{label}
 </button>
 
 <style lang="postcss">
@@ -20,23 +27,24 @@
 		border: 1px outset var(--light-blue);
 		background-color: var(--light-blue);
 		color: white;
-		height: 38px;
 		cursor: pointer;
 		transition: box-shadow 0.1s;
 	}
-
+	
 	.btn:active {
 		border: 1px outset var(--med-blue);
 		background-color: var(--med-blue);
 	}
-
+	
 	.btn:hover {
 		box-shadow: 0 0 11px rgba(33, 33, 33, 0.2);
 	}
-
+	
 	.disabled,
 	.disabled:active,
 	.disabled:hover {
+		box-shadow: none;
+		cursor: inherit;
 		background-color: grey;
 		border: grey;
 	}
