44bb773984d30b1fbdf7c604fb3c4c8d90ad6541
James Bradbury <me@jamesbradbury.net>
2022-01-19 17:20:36 +0000

    refactor

--- src/lib/widget/SpectralShape.svelte
+++ src/lib/widget/SpectralShape.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { Chart, registerables } from 'chart.js';
 	import * as Meyda from 'meyda';
 	import * as Tone from 'tone';
-	
+	import { binToHz } from '$lib/utils';
+
+	import Button from '$lib/components/Button.svelte'	
 	import SourceGenerator from './SourceGenerator.svelte';
-import { enableProdMode } from '@tensorflow/tfjs';
+	import Spectrogram from './Spectrogram.svelte';
 	
 	const windowSize = 512;
 	const bufferSize = windowSize;
-	let spectrum = new Array(windowSize).fill(0.0);
-	const binLabels = new Array(windowSize/2).fill(null).map((_, i) => i * (44100/windowSize))
-	console.log(binLabels)
+
+	// Visuals
+	let chart;
 	
 	// Audio
 	let audioContext: AudioContext;
@@ -29,85 +29,11 @@ import { enableProdMode } from '@tensorflow/tfjs';
 		'crest' : 0
 	}
 	
-	// Canvas
-	let canvas: HTMLCanvasElement | null;
-	let ctx: RenderingContext;
-	let chart: Chart;
-	
-	// Control
-	let smoothing = 150;
-	
-	onMount(async () => {
-		// Chart
-		Chart.register(...registerables);
-		ctx = canvas.getContext('2d');
-		const data = {
-			labels: binLabels,
-			datasets: [
-			{
-				data: spectrum,
-				borderColor: '#000',
-				borderWidth: 0.8,
-				cubicInterpolationMode: 'monotone',
-				tension: 0.1,
-				pointRadius: 0,
-			}
-			]
-		};
-		chart = new Chart(ctx, {
-			type: 'line',
-			data: data,
-			options: {
-				plugins: {
-					legend: { display: false },
-					tooltip: {
-						enabled: false,
-					}
-				},
-				datasets: {
-					line: {
-						pointRadius: 0
-					}
-				},
-				elements: {
-					point: {
-						radius: 0
-					}
-				},
-				normalized: true,
-				responsive: true,
-				maintainAspectRatio: false,
-				animation: { duration: smoothing },
-				scales: {
-					x: {
-						display: true,
-						type: 'logarithmic',
-						min: 20,
-						max: 20000
-					},
-					y: {
-						display: false,
-						type: 'logarithmic',
-						min: 0,
-						max: 10000,
-						
-					}
-				}
-			}
-		});
-	});
-	
-	function binToHz(bin, windowSize, sampleRate) {
-		const binWidth = sampleRate / windowSize;
-		return bin * binWidth;
-	}
-	
 	function startAudioDescriptor() {
 		if (!enabled) {
 			Tone.start();
 			generator.patch("osc"); // activate the sine tone generator
-		} else {
-			audioContext.close();
+			enabled = true
 		}
 		
 		// Provide with the Tone.js audio context;
@@ -123,57 +49,55 @@ import { enableProdMode } from '@tensorflow/tfjs';
 			'spectralKurtosis',
 			'spectralRolloff',
 			'spectralFlatness',
-			
 			],
 			callback: (...features) => {
 				const desc = features[0];
-				spectrum = desc.amplitudeSpectrum;
+				const spectrum = desc.amplitudeSpectrum;
+				chart.data.datasets[0].data = spectrum;				
 				shape.centroid = binToHz(desc.spectralCentroid, windowSize, 44100);
 				shape.spread = desc.spectralSpread;
 				shape.skewness = desc.spectralSkewness;
 				shape.kurtosis = desc.spectralKurtosis;
 				shape.rolloff = desc.spectralRolloff;
 				shape.flatness = desc.spectralFlatness;
-				chart.data.datasets[0].data = spectrum;				
 				chart.update();
 			}
 		});
 		analyser.start();
 	};
+
+	$: if (chart) chart.options.plugins.annotation.annotations.centroid.xMin = shape.centroid;
+	$: if (chart) chart.options.plugins.annotation.annotations.centroid.xMax = shape.centroid;
+	$: if (chart) chart.options.plugins.annotation.annotations.rolloff.xMin = Math.min(shape.rolloff, 20000);
+	$: if (chart) chart.options.plugins.annotation.annotations.rolloff.xMax = Math.min(shape.rolloff, 20000);
+
 </script>
 
 <div class="container">
-	<button on:click={startAudioDescriptor}>
-		Start Audio
-	</button>
-	
-	<canvas id="vis" bind:this={canvas} />
+	<Button on:click={startAudioDescriptor}
+	label='Start Audio'
+	width = 'max-content'
+	height = 'max-content'
+	disabled={enabled}
+	/>
+	<br>
 
-	<button on:click={
-		() => {
-			chart.options.scales.x.type='linear'
-		}
-	}>lin</button>
-	
-	<button on:click={
-		() => {
-			chart.options.scales.x.type='logarithmic'
-		}
-	}>log</button>
-	
-	{#each Object.entries(shape) as [k, v]}
-	<div class="descriptor">
-		<div>{ k }: { v.toFixed(1) }</div>
-	</div>
-	{/each}
-	
-	
+	<Spectrogram bind:chart={chart}/>
+
 	<SourceGenerator 
 	bind:audioContext={audioContext}
 	bind:output={source}
 	bind:this={generator}
 	/>
-	
+
+	<div class="values">
+		{#each Object.entries(shape) as [k, v]}
+		<div class="descriptor">
+			<div>{ k }:</div>
+			<div>{ v.toFixed(1) }</div>
+		</div>
+		{/each}
+	</div>
 </div>
 
 <style>
@@ -183,11 +107,14 @@ import { enableProdMode } from '@tensorflow/tfjs';
 		margin-top: 1em;
 		margin-bottom: 1em;
 	}
-	
-	#vis {
-		width: 99% !important;
-		min-width: 0 !important;
-		max-height: 280px;
-		margin: 0 auto;
+
+	.values {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.descriptor {
+		display: grid;
+		grid-template-columns: 80px auto;
 	}
 </style>
