56fbd415ec922445f33fbbab09e6af3428559cf1
James Bradbury <me@jamesbradbury.net>
2022-01-21 15:31:05 +0000

    update grid component

--- src/lib/widget/Grid.svelte
+++ src/lib/widget/Grid.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 	import { Chart, registerables } from 'chart.js';
-
+    import { interpolateSinebow } from 'd3-scale-chromatic';
+    import Button from '$lib/components/Button.svelte';
+    import gaussian4 from '../../../static/data/gaussian4.json';
+    import out from '../../../static/ref/grid/out.json';
+    
     let canvas: HTMLCanvasElement;
     let ctx: RenderingContext;
     let chart: Chart;
 
-    let positions = new Array(100).fill(new Array(2).fill(0.0))
-    positions = positions.map(x => [Math.random(), Math.random()] )
+    const raw = Object.values(gaussian4.data).map(x => x);
+    const grid= Object.values(out.data).map(x => x);
+    const colours = raw.map(x => interpolateSinebow(x[0]*x[1]))
+    let positions = raw;
 
-    function align() {
 
-    }
-
-    
     onMount(async () => {
         Chart.register(...registerables);
 		ctx = canvas.getContext('2d');
@@ -21,8 +23,9 @@
         const data = {
             datasets: [
                 { 
-                    data: positions,
-                    backgroundColor: []
+                    data: raw,
+                    backgroundColor: colours,
+                    pointRadius: 6,
                 }
             ]
         }
@@ -34,23 +37,63 @@
                 responsive: true,
                 maintainAspectRatio: false,
                 plugins: { legend: { display: false }},
-                animation: { duration: 100 },
+                animation: { duration: 0 },
                 scales: { 
                     x: { display: false },
                     y: { display: false }
                 }
             }
         })
+        chart.options.animation = { duration: 1000}
 
     })
 </script>
+<div class='container'>
+    <canvas bind:this={canvas} id="scatter-plot" />
 
-<canvas bind:this={canvas} id="scatter-plot" />
+    <div id="controls">
+        <Button
+        width={'max-content'}
+        on:click={
+            () => {
+                chart.data.datasets[0].data = grid;
+                chart.update();
+            } }
+            label='Assign to Grid'
+        />
+        
+        <Button
+        width={'max-content'}
+        on:click={ 
+            () => {
+                chart.data.datasets[0].data = raw;
+                chart.update();
+            } 
+        }
+        label='Original State'
+        />
+    </div>
+    
+</div>
+
 
 <style>
+
+    .container {
+        margin-bottom: 1em;
+    }
     #scatter-plot {
 		max-height: 400px;
 		max-width: 100%;
 		border: 1px solid rgb(232, 232, 232);
+        border-radius: 0.25em;
+    }
+
+    #controls {
+        display: grid;
+        grid-template-columns: auto auto;
+        justify-content:center;
+        gap: 1em;
+        margin-top: 1em;
     }
 </style>
