118f734376fd1bd871cd19149b49d116b3043c0a
James Bradbury <me@jamesbradbury.net>
2022-01-24 12:24:39 +0000

    add a labelsetview component

---
+++ src/lib/components/LabelSetView.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let data = [];
+</script>
+
+<div class="table">
+	<div class="heading">Identifier</div>
+	<div class="heading">Label</div>
+	{#each data as point}
+		{#each Object.entries(point) as [id, label]}
+			<div class="entry">{id}</div>
+			<div class="entry">{label}</div>
+		{/each}
+	{/each}
+</div>
+
+<style lang="postcss">
+	.table {
+		display: grid;
+		grid-template-columns: auto auto;
+		width: min(20ch, 100%);
+		font-family: var(--mono);
+		gap: 1em;
+		margin-bottom: 1em;
+	}
+
+	.heading {
+		font-weight: bold;
+	}
+
+	.entry {
+		color: grey;
+		text-align: left;
+	}
+</style>
