dc9f33f8d2b45e4b5dfd816344617a88347c7853
James Bradbury <github@jamesbradbury.net>
2022-01-26 18:35:22 +0000

    [Content] Median Filters Overview (#138)
    * add median-filtering skeleton
    
    * add images and video
    
    * remove whitespace
    
    * create a video component for mp4/webm
    
    * make webm and mp4 binaries for git
    
    * add assets
    
    * add pd patches as binary to gitattr
    
    * update video component to be more user friendly
    
    * update text according to ted's suggestions
    
    * nuance text

---
+++ src/lib/components/Video.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    export let url: string = '';
+    export let label= "";
+
+    url = url.replace(/\.[^/.]+$/, ""); // remove file extension
+</script>
+
+<figure>
+    <video controls loop>
+        <source src={`${url}.mp4`} type="video/mp4">
+        <source src={`${url}.webm`} type="video/webm">
+        <p>Your browser doesn't support HTML5 video. </p>
+        <track kind=captions>
+    </video>
+
+    <figcaption class="caption">
+        {label}
+    </figcaption>
+
+</figure>
+
+<style>
+    figure {
+        display: grid;
+        grid-template-rows: auto auto;
+        place-items: center;
+        gap: 1em;
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+    video {
+        max-width: 100%;
+    }
+
+    .caption {
+		font-size: 0.75rem;
+    }
+</style>
