01c15c391212a544768aba4c025dba67b1fd2e24
James Bradbury <me@jamesbradbury.net>
2022-02-03 13:26:08 +0000

    get npy data for the filter curve

--- src/lib/widget/Loudness.svelte
+++ src/lib/widget/Loudness.svelte
@@ -1,4 +1,27 @@
 <script>
+	import { fromArrayBuffer } from 'numpy-parser';
+    import { onMount } from 'svelte';
+ 
+    const db = '/reference/loudness/db.npy'
+    const freq = '/reference/loudness/filter.npy'
+    
+    let y = [];
+    let x = [];
 
+    onMount(async () => {
+        fetch(db)
+        .then((response) => response.arrayBuffer())
+        .then((arr) => {
+            y = fromArrayBuffer(arr).data;
+        });
+
+        fetch(freq)
+        .then((response) => response.arrayBuffer())
+        .then((arr) => {
+            x = fromArrayBuffer(arr).data;
+        })
+    })
 </script>
+
+{ x}
 
