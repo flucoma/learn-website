d9e216af4c8e8dfb21326a45a0d35098425e9319
James Bradbury <me@jamesbradbury.net>
2022-02-09 13:53:35 +0000

    [Enhance] Related articles under table of contents (#210)
    * add related-links to sidebar
    
    * update scripts
    
    * add related scripts

--- src/lib/components/TOC.svelte
+++ src/lib/components/TOC.svelte
@@ -1,32 +1,34 @@
 <script>
 	import { page } from '$app/stores';
 	import { structure } from '$lib/app.js';
+	import Related from '$lib/components/Related.svelte'
 
 	$: headings = $structure[$page.url.pathname] || [];
 </script>
 
 {#if headings.length > 0}
 <nav class="container">
-	<div class="toc">Table of Contents</div>
-	<div class="headings">
-		{#each headings as h}
+	<div>
+		<h3 class="toc">Table of Contents</h3>
+		<div class="headings">
+			{#each headings as h}
 			<a target='_self' href={h.url}>{h.text}</a>
-		{/each}
+			{/each}
+		</div>
 	</div>
+	<Related />
 </nav>
 {/if}
 
+
 <style lang="postcss">
 	.container {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
 		position: fixed;
 		z-index: 1;
 		max-width: 23ch;
-	}
-
-	.toc {
-		font-size: 1rem;
-		margin-bottom: 1em;
-		font-weight: bold;
 	}
 
 	.headings {
@@ -43,6 +45,10 @@
 	@media (max-width: 1200px) {
 		.container {
 			position: relative;
+			display: grid;
+			grid-template-columns: auto auto;
+			max-width: 100%;
+			width: 100%;
 		}
 	}
 </style>
