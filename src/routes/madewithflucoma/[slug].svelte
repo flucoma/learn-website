265f53db5ca2b30aed76bfe3bc18685a4d2b98da
James Bradbury <me@jamesbradbury.net>
2022-02-14 11:59:00 +0000

    setup redirects for old naming system

---
+++ src/routes/madewithflucoma/[slug].svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	export async function load({ params }) {
+		const { slug } = params;
+        return { 
+            props: { slug } 
+        };
+	}
+</script>
+
+<script>
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+    export let slug;
+
+    onMount(async() => {
+        goto(`/explore/${slug}`);
+    })
+</script>
