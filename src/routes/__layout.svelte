bd18fd69aa291290b176c4fdd21b81746a17d9fd
Jacob <jacob.dchart@gmail.com>
2022-02-17 10:25:37 +0100

    Modified top-level __layout
    Only contains the header, footer and crumbs. Everything is wrapped in the "content" div

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,6 @@
 	import Header from '$lib/Header.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import Crumbs from '$lib/components/Crumbs.svelte';
-	import TOC from '$lib/components/TOC.svelte';
 </script>
 
 <svelte:head>
@@ -17,17 +16,11 @@
 	<Crumbs />
 
 	<div class="content">
-		<div class="navigation">
-			<TOC />
-		</div>
 
-		<main class="main">
-			<slot />
-		</main>
+		<slot />
 
-		<div class="related">
-		</div>
 	</div>
+	
 	<Footer />
 </div>
 
@@ -47,26 +40,6 @@
 		flex: 1 0 auto;
 	}
 
-	.main {
-		grid-area: main;
-		min-width: var(--min-text-width);
-		max-width: var(--max-text-width);
-		display: flex;
-		flex-direction: column;
-	}
-
-	.navigation {
-		grid-area: navigation;
-		width: 25ch;
-		height: max-content;
-	}
-
-	.related {
-		grid-area: related;
-		width: 25ch;
-	}
-
-	/* Media Queries */
 	@media (min-width: 1200px) {
 		.content {
 			grid-template-columns: auto min(var(--max-text-width), 100%) auto;
@@ -80,14 +53,6 @@
 			grid-template-areas:
 				'navigation'
 				'main';
-		}
-
-		.navigation {
-			width: 100%;
-		}
-
-		.related {
-			display: none;
 		}
 	}
 </style>
