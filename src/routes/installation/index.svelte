30ee372e246188365fa881bddab6c5ff070bd70e
Jacob <jacob.dchart@gmail.com>
2022-02-22 10:14:19 +0100

    Making installation index look how it used to

--- src/routes/installation/index.svelte
+++ src/routes/installation/index.svelte
@@ -1,21 +1,63 @@
-<h1>Choose your creative coding environment</h1>
+<div class="navigation">
 
-<div class="installations">
-	<a  href="/installation/max">
-		<h2>Max</h2>
-		<img class="max" src="/img/max-logo.svg" alt="Max Logo" />
-	</a>
-	<a  href="/installation/sc">
-		<h2>SuperCollider</h2>
-		<img src="/img/sc-logo.svg" alt="SuperCollider Logo" />
-	</a>
-	<a  href="/installation/pd">
-		<h2>PureData</h2>
-		<img src="/img/pd-logo.png" alt="PureData Logo" />
-	</a>
+</div>
+
+<main class="main">
+
+    <h1>Choose your creative coding environment</h1>
+
+	<div class="installations">
+		<a  href="/installation/max">
+			<h2>Max</h2>
+			<img class="max" src="/img/max-logo.svg" alt="Max Logo" />
+		</a>
+		<a  href="/installation/sc">
+			<h2>SuperCollider</h2>
+			<img src="/img/sc-logo.svg" alt="SuperCollider Logo" />
+		</a>
+		<a  href="/installation/pd">
+			<h2>PureData</h2>
+			<img src="/img/pd-logo.png" alt="PureData Logo" />
+		</a>
+	</div>
+
+</main>
+
+<div class="related">
+    
 </div>
 
 <style>
+
+.main {
+        grid-area: main;
+        min-width: var(--min-text-width);
+        max-width: var(--max-text-width);
+        display: flex;
+        flex-direction: column;
+    }
+
+    .navigation {
+        grid-area: navigation;
+        width: 25ch;
+        height: max-content;
+    }
+
+    .related {
+        grid-area: related;
+        width: 25ch;
+    }
+
+    @media (max-width: 1200px) {
+        .navigation {
+            width: 100%;
+        }
+
+        .related {
+            display: none;
+        }
+    }
+
 	.installations {
 		display: flex;
 		flex-direction: column;
