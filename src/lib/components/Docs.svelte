c4f23f49e749ef6b976b8b45bffa4f6fa8e6019c
James Bradbury <github@jamesbradbury.net>
2022-02-28 16:00:04 +0000

    add a Docs compoennt

---
+++ src/lib/components/Docs.svelte
@@ -0,0 +1,20 @@
+<script>
+    
+    export let obj;
+    
+    const get_docs = async() => {
+        const res = await fetch('https://learn.flucoma.org/api.json')
+        const obj = await res.json();
+        if (res.ok) {
+            return obj
+        } else {
+            throw new Error(obj)
+        }
+    }
+</script>
+
+{#await get_docs()}
+<p>Loading...</p>
+{:then docs}
+<p>{ docs[obj].client_name }</p>
+{/await}
