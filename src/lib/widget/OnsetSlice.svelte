5650dcdeddc07db15d8d02dd7ddb02d7f38e9074
James Bradbury <github@jamesbradbury.net>
2022-03-02 10:08:07 +0000

    add onsetslice numpy parsing widget

---
+++ src/lib/widget/OnsetSlice.svelte
@@ -0,0 +1,18 @@
+<script>
+
+import { fromArrayBuffer } from 'numpy-parser';
+import { onMount } from 'svelte';
+
+let d;
+
+onMount(async() => {
+    fetch('/ref/onsetslice/mags.npy')
+    .then(res => res.arrayBuffer())
+    .then(arr => {
+        const raw = fromArrayBuffer(arr)
+        const shape = raw.shape;
+        const data = raw.data;
+    })
+})
+
+</script>
