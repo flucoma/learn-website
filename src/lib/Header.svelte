be189a47226608d2cfacbddbfe12a071740d08d2
James Bradbury <github@jamesbradbury.net>
2022-03-02 17:54:00 +0000

    popover navigation components

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,72 +1,36 @@
 <script>
 	import Search from '$lib/components/Search.svelte';
 	import Hamburger from '$lib/components/Hamburger.svelte';
-	let w;
-	const breakpoint = 1200;
-	let userExpand = false;
-	const expandHandler = () => {
-		userExpand = !userExpand;
-	};
+	import PopoverNav from '$lib/components/PopoverNav.svelte';
+	import { nav_expanded } from '$lib/app';
 
-	$: navExpand = w >= breakpoint || userExpand;
+	const expandHandler = () => { $nav_expanded = !$nav_expanded };
 </script>
 
-<header class="container" role="navigation">
-	{#if w < breakpoint}
-		<Hamburger on:click={expandHandler} />
-	{/if}
-	{#if navExpand}
-		<div class="links">
-			{#if w >= breakpoint}
-				<a class="logo-link" href="/">
-					<img class="logo" src="/img/onlylogo.svg" alt="FluCoMa Logo" />
-				</a>
-			{/if}
-			<a  class="nav-link" href="/learn">Learn</a>
-			<a  class="nav-link" href="/explore">Explore</a>
-			<a  class="nav-link" href="/reference">Reference</a>
-		</div>
-	{/if}
+<header>
+	<Hamburger on:click={expandHandler} />
 	<Search />
+	<a class="logo-link" href="/">
+		<img class="logo" src="/img/onlylogo.svg" alt="FluCoMa Logo" />
+	</a>
 </header>
 
-<svelte:window bind:innerWidth={w} />
+<PopoverNav />
 
 <style lang="postcss">
-	.container {
+	header {
 		display: grid;
-		grid-template-columns: auto auto;
+		grid-template-columns: auto auto auto;
 		place-items: center;
 		background: var(--dark-blue);
-		padding: 1em;
 		gap: 3em;
-	}
-
-	@media (max-width: 1200px) {
-		.container {
-			display: flex;
-			flex-direction: column;
-			gap: 0.5em;
-		}
-	}
-
-	.links {
-		display: flex;
-		flex-direction: row;
-		margin: 0 auto;
-		gap: 1em;
-	}
-
-	@media (max-width: 1200px) {
-		.links {
-			flex-direction: column;
-			gap: 0.3em;
-		}
+		height: 50px;
 	}
 
 	.logo {
 		width: 2em;
-		height: 100%;
+		height: 2.2em;
+		margin-top: 0.3em;
 	}
 
 	.logo-link {
@@ -76,28 +40,5 @@
 
 	.logo-link:hover {
 		background: transparent;
-	}
-
-	.nav-link {
-		font-family: var(--font);
-		margin-top: auto;
-		margin-bottom: auto;
-		text-align: center;
-		font-size: 1.5em;
-		color: white;
-		text-decoration: none;
-	}
-
-	.nav-link:hover {
-		background: transparent;
-		text-decoration: underline;
-	}
-
-	.nav-link:visited {
-		color: white;
-	}
-
-	.nav-link:visited {
-		color: white;
 	}
 </style>
---
+++ src/lib/components/PopoverNav.svelte
@@ -0,0 +1,56 @@
+<script>
+	import Landing from '$lib/components/cards/Landing.svelte';
+	import { fly } from 'svelte/transition';
+	import { nav_expanded } from '$lib/app';
+</script>
+
+{#if $nav_expanded}
+<nav class='popover' 
+transition:fly={{ duration:200, y:0, x:-200 }}
+>
+	<h2>Navigation</h2>
+	<Landing link='/learn'>
+		<h2 slot="title">Learn</h2>
+		<p slot="blurb" class='blurb'>
+		A variety of articles and guides discussing machine listening and machine learning, sometimes relating to specific FluCoMa technology and sometimes reflecting more broadly.
+		</p>
+	</Landing>
+	
+	<Landing link='/reference'>
+		<h2 slot="title">References</h2>
+		<p slot="blurb" class='blurb'>
+		The references outline the underlying algorithms of the FluCoMa toolkit in more detail. We try to steer clear from specific implementation details and instead focus on the concepts behind each algorithm and to help illuminate what goes on under the hood.
+		</p>
+	</Landing>
+	
+	<Landing link='/explore'>
+		<h2 slot="title">Explore</h2>
+		<p slot="blurb" class='blurb'>
+		Made with FluCoMa draws attention to people like you. Makers who use the FluCoMa toolkit or are involved in the wider community. We want to highlight what is out there to try and cultivate opportunities for learning and to situate this technology within a community of techno-fluent musicians.
+		</p>
+	</Landing>
+</nav>
+{/if}
+
+<style>
+    .popover {
+		opacity: 1;
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+		position: fixed;
+		top: 50px; left: 0;
+		background-color: white;
+		opacity: 1;
+		width: min(500px, 50%);
+		height: 100vh;
+		text-align: left;
+		padding: 1.5em;
+		z-index: 99;
+	}
+
+	.blurb {
+		color: grey;
+		text-align: left;
+	}
+</style>
