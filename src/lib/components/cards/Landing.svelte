4b5aaa1e0699daa36c07483ac5f07ab50b54a413
James Bradbury <github@jamesbradbury.net>
2022-03-03 16:44:25 +0000

    always reset popover if navigate is successful

--- src/lib/components/cards/Landing.svelte
+++ src/lib/components/cards/Landing.svelte
@@ -1,12 +1,13 @@
 <script>
     import ArrowRight from '$lib/components/ArrowRight.svelte';
+    import { nav_expanded } from '$lib/app';
     export let link = '/'
 </script>
 
 <div class="container">
     <slot name='title' />
     <slot name='blurb' />
-    <a class='linkout' href={link}>Learn More <ArrowRight /></a>
+    <a on:click={() => { $nav_expanded = false }} class='linkout' href={link}>Learn More <ArrowRight /></a>
 </div>
 
 <style>
