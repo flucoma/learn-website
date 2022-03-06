20320536f5591fb9a6ab2043981d2e104ae92245
James Bradbury <github@jamesbradbury.net>
2022-03-06 10:59:54 +0000

    prototype of jargon working

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,12 @@ import slug from 'rehype-slug';
 import headings from 'rehype-autolink-headings';
 import { s } from 'hastscript';
 import { mdsvex } from 'mdsvex';
+import jargon from 'remark-jargon';
+
+const jg = {
+    remark: "<b>remark</b> is a markdown processor powered by plugins. See <a href='https://remark.js.org/'>remark.js.org</a>.",
+    freesewing: "<b>FreeSewing</b> is an open source platform for made-to-measure sewing patterns. See <a href='https://freesewing.org/'>freesewing.org</a>"
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -37,6 +43,9 @@ const config = {
 						])
 					},
 				]
+			],
+			remarkPlugins: [
+				[jargon, { jargon: jg }]
 			],
 			layout: {
 				_: './src/lib/layouts/Content.svelte',
