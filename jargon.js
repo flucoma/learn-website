20caf07f7c47f06ad94e9fdc83548430740e6d44
Ted Moore <ted@tedmooremusic.com>
2022-03-24 15:51:25 +0000

    BufStats ⚠️

--- jargon.js
+++ jargon.js
@@ -3,9 +3,8 @@
 // WARNING
 
 const definitions = {
-    spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>."
+    spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>.",
+    interquartile: "The interquartile range is the range between the 25th and 75th [percentiles](/reference/bufstats/#percentiles). 50% of the data will fall within this range."
 }
-
-
 
 export default { definitions };
