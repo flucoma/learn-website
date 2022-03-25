a7b0a66a74b84a9c9ef25af50233f485498a4844
Jacob <jacob.dchart@gmail.com>
2022-03-25 13:11:06 +0100

    toc-adjustents

--- src/lib/components/TOC.svelte
+++ src/lib/components/TOC.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { page } from '$app/stores';
 	import { structure } from '$lib/app.js';
+	import { onMount } from 'svelte';
 	import Related from '$lib/components/Related.svelte'
 
 	let headings = [];
@@ -9,9 +10,50 @@
 		path = path.endsWith('/') ? path.slice(0, -1) : path;
 		headings = $structure[path] || [];
 	}
+
+	let toc_container;
+	let scrollY = 0;
+	let original_offset = 0;
+
+	var can_exec = false;
+	$: handle_scroll(scrollY);
+
+	function handle_scroll(y_offset){
+		if(can_exec){
+			if(window.matchMedia("(min-width: 1200px)").matches){
+				if (y_offset >= 100){
+					toc_container.style.top = '0px';
+					toc_container.style.maxHeight = '100%';
+				}
+				else{
+					toc_container.style.top = String(original_offset) + 'px';
+					toc_container.style.maxHeight = '80%';
+				};
+			}
+			else{
+
+				toc_container.style.top = "0px";
+				toc_container.style.maxHeight = "100%";
+				toc_container.style.overflowY = "none";
+			}
+		};
+		can_exec = true;
+	}
+
+	onMount(async () => {
+		can_exec = true;
+		original_offset = toc_container.offsetTop;
+
+		function reportWindowSize() {
+			handle_scroll(scrollY)
+		}
+
+		window.onresize = reportWindowSize;
+	});
+
 </script>
 
-<nav class="container">
+<nav class="container" bind:this={toc_container}>
 	<div>
 		{#if headings.length > 0}
 		<h3 class="toc">Table of Contents</h3>
@@ -25,6 +67,7 @@
 	<Related />
 </nav>
 
+<svelte:window bind:scrollY={scrollY} />
 
 <style lang="postcss">
 	.container {
@@ -34,6 +77,13 @@
 		position: fixed;
 		z-index: 1;
 		max-width: 23ch;
+		
+		top: auto;
+		max-height: 80%;
+		overflow-y: scroll;
+
+		transition-property: top;
+		transition-duration: 1s;
 	}
 
 	.headings {
@@ -54,6 +104,10 @@
 			grid-template-columns: auto auto;
 			max-width: 100%;
 			width: 100%;
+
+			top: auto;
+			max-height: 100%;
+			overflow-y: none;
 		}
 	}
 </style>
