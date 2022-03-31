13803f99fcebbf3b2d1105e770c4fbf283b8ae1b
James Bradbury <github@jamesbradbury.net>
2022-03-31 18:27:21 +0100

    add a component for inserting related links

---
+++ src/lib/components/RelatedLinks.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Related from '$lib/components/Related.svelte'
+</script>
+
+<nav class="container">
+	<Related />
+</nav>
+
+<svelte:window />
+
+<style lang="postcss">
+
+</style>
