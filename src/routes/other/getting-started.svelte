56ebb68099be3fe2af066a8b6bbd19f2071e8a70
Jacob <jacob.dchart@gmail.com>
2022-04-01 15:18:35 +0200

    More flucoma-y placeholder stuff

--- src/routes/other/getting-started.svelte
+++ src/routes/other/getting-started.svelte
@@ -1,6 +1,9 @@
 <script>
     import InteractiveNetwork from '$lib/components/InteractiveNetwork.svelte';
+    import ArrowRight from '$lib/components/ArrowRight.svelte';
 </script>
+
+
 
 <div class="main_wrapper">
 
@@ -15,13 +18,14 @@
         
         <div class="network_texts" style="grid-area: network_texts;">
             <h3>The FluCoMa Network</h3>
-            <p>
-                Explore the FluCoMa universe...
-            </p>
+            <p>Explore the FluCoMa universe...</p>
 
-            <p id="display_name">
-                Testing content change...
-            </p>
+            <h4 id="display_name">FluCoMa</h4>
+
+            <p id="display_blurb">FluCoMa stands for Fluid Corpus Manipulation. It's all about dealing with large amounts of digital audio.</p>
+
+            <a class='linkout' id="display_link" href="/">Learn More <ArrowRight /></a>
+
         </div>
     </div>
 
@@ -35,27 +39,22 @@
                     <a href="/installation">Installation</a>
                     <p>Start here to install stuff otherwise you wont be able to use anything...</p>
                 </li>
-
                 <li>
                     <a href="/installation">Getting started basic tutorial video looooool</a>
                     <p>Learn all the basics about working in the FluCoMa framework...</p>
                 </li>
-
                 <li>
                     <a href="/installation">MLP Classifier</a>
                     <p>Get started using machine learning by building an MLP classifier.</p>
                 </li>
-
                 <li>
                     <a href="/installation">MLP Regressor</a>
                     <p>Take neural networks a step further with regression.</p>
                 </li>
-
                 <li>
                     <a href="/installation">2d visulaisation vid</a>
                     <p>Confused about a bunch of noisy crap? Make a 2d visualiser and fun.</p>
                 </li>
-
                 <li>
                     <a href="/installation">Audio query</a>
                     <p>LEarn whjat an audio is.</p>
@@ -69,17 +68,19 @@
 <style>
 
     .main_wrapper{
-		position: relative;
-		left: 0;
-		width: 100%;
-		height: 100%;
+        width: 100%;
+        height: 100%;
+        min-width: 100%;
     }
 
     .network_div{
+        width: 100%;
+        min-width: 100%;
         display: grid;
         grid-template-columns: repeat(2, 1fr);
         grid-template-areas: 
         "network_vis network_texts";
+        align-items: center;
     }
 
     .network_texts{
@@ -89,27 +90,56 @@
 
     .roadmap_div{
         width: 100%;
-        margin-left: 1em;
-        margin-right: 1em;
+        min-width: 100%;
     }
 
     @media (min-width: 1200px){
         .network_div{
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-areas: 
-        "network_vis network_texts";
-    }
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-areas: 
+            "network_vis network_texts";
+        }
+
+        .roadmap_div{
+            width: 100%;
+            min-width: 100%;
+        }
     }
 
     @media (max-width: 1200px) {
         .network_div{
-        display: grid;
-        grid-template-columns: repeat(1, 1fr);
-        grid-template-areas: 
-        "network_vis"
-        "network_texts";
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(1, 1fr);
+            grid-template-areas: 
+            "network_vis"
+            "network_texts";
+        }
+
+        .roadmap_div{
+            width: 100%;
+            min-width: 100%;
+        }
     }
+
+    .linkout {
+        display: grid;
+        grid-template-columns: auto auto;
+        justify-content: left;
+        place-items: center;
+        gap: 0.2em;
+    }
+
+    .linkout:hover {
+        background-color: transparent;
+        color: var(--med-blue);
+        gap: 0.3em;
+    }
+
+    .linkout, .linkout:hover {
+        transition: gap 80ms ease-in-out;
     }
 
 </style>
