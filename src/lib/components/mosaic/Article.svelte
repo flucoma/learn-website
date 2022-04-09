0ac671d9881c2f1407e8cf9bd02d8a32f583d0ae
James Bradbury <github@jamesbradbury.net>
2022-04-09 15:23:08 +0800

    update styles of mosaic tiles

--- src/lib/components/mosaic/Article.svelte
+++ src/lib/components/mosaic/Article.svelte
@@ -2,7 +2,7 @@
     import ArrowRight from '$lib/components/ArrowRight.svelte';
     import Flair from '$lib/components/Flair.svelte';
     import _ from 'lodash';
-
+    
     type ArticleData = {
         title: string,
         blurb: string,
@@ -14,60 +14,68 @@
         crumb?: string,
         related?: string
     }
-
+    
     export let data: ArticleData = {};
 </script>
 
 <div class="container raisedbox">
-
+    
     <a href="{data.url}">
-    
-    <div class="img_container">
-        <div class='feature_image' style="background-image: url({data.feature.featuredimage || _.sample(data.feature.images) || '/general/learn_default.jpeg' });"></div>
-    </div>
-
-    <div class="text_cont">
-        <h2>{data.title}</h2>
-        <p>{data.blurb}</p>
-    
-        <div class="flair_link_wrapper">
-            <div class="flair_wrapper"><Flair flair={data.flair}/></div>
-            <a  class='linkout' href={data.url}>Learn More <ArrowRight /></a>
+        
+        <div class="img_container">
+            <div class='feature_image' style="background-image: url({data.feature.featuredimage || _.sample(data.feature.images) || '/general/learn_default.jpeg' });"></div>
         </div>
         
-    </div>
-
-</a>
-
+        <div class="text_cont">
+            <h2>{data.title}</h2>
+            <p class='blurb'>{data.blurb}</p>
+            
+            <div class="flair_link_wrapper">
+                <div class="flair_wrapper"><Flair flair={data.flair}/></div>
+                <a  class='linkout' href={data.url}>Learn More <ArrowRight /></a>
+            </div>
+            
+        </div>
+        
+    </a>
+    
 </div>
 
 <style>
 
-.container >a{
-    color: black;
-    text-decoration: none;
-    width: 100%;
-}
+    .blurb {
+        font-size: 0.8rem;
+        color: #595959;
 
+    }
+    
+    .container >a{
+        color: black;
+        text-decoration: none;
+        width: 100%;
+    }
+    
     .flair_link_wrapper{
         display: flex;
+        flex-direction: row;
+        align-items: center  ;
         
     }
-
+    
     .flair_wrapper{
         width: fit-content;
     }
-
+    
     .text_cont{
         margin: 0.75em;
     }
-
+    
     .img_container{ 
         background-repeat: no-repeat;
         background-size: cover;
         width: 100%;
     }
-
+    
     .feature_image {
         position: relative;
         width: 100%;
@@ -76,7 +84,7 @@
         background-repeat: no-repeat;
         background-size: cover; 
     }
-
+    
     .container {
         border-radius: 0.25rem;
         border: 0.063rem solid #dcdee0;
@@ -86,24 +94,24 @@
         margin-bottom: 1em;
         width: 100%;
     }
-
+    
     .container > p {
         color: grey;
         text-align: justify;
         text-justify: inter-word;
     }
-
+    
     /* Overwrite hover behaviour */
     .container, .container:hover {
         color:black;
         background-color: white;
         text-decoration: none;
     }
-
+    
     .container:hover {
         cursor: pointer;
     }
-
+    
     .linkout {
         display: grid;
         grid-template-columns: auto auto;
@@ -112,13 +120,13 @@
         gap: 0.2em;
         padding-left: 1em;
     }
-
+    
     .linkout:hover {
         background-color: transparent;
         color: var(--med-blue);
         gap: 0.3em;
     }
-
+    
     .linkout, .linkout:hover {
         transition: gap 80ms ease-in-out;
     }
