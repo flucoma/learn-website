28812e36880e132b3cee8e3af1f0d78ac7485df9
James Bradbury <github@jamesbradbury.net>
2022-04-30 15:57:32 +0800

    use the mosaic list

--- src/lib/components/mosaic/List.svelte
+++ src/lib/components/mosaic/List.svelte
@@ -5,61 +5,24 @@ References are slightly different and use the ReferenceEntry component.
 <script>
 	import { docs } from '$lib/app.js';
 	import Article from '$lib/components/mosaic/Article.svelte';
+	import Masonry from 'svelte-bricks';
+	
 	export let section = 'learn';
+	
+	let [minColWidth, maxColWidth, gap] = [200, 800, 20]
+	let width, height;
 
 	const items = docs.filter((x) => x.section === section);
 </script>
 
-<div class="overview-list">
-
-	<div class="list_col">
-		{#each items as item, i}
-			{#if i % 3 == 0}
-				<Article data={item}/>
-			{/if}
-		{/each}
-	</div>
-
-	<div class="list_col">
-		{#each items as item, i}
-			{#if i % 3 == 1}
-				<Article data={item}/>
-			{/if}
-		{/each}
-	</div>
-
-	<div class="list_col">
-		{#each items as item, i}
-			{#if i % 3 == 2}
-				<Article data={item}/>
-			{/if}
-		{/each}
-	</div>
-
-
-</div>
-
-<style lang="postcss">
-
-	.list_col{
-		width: 100%;
-	}
-
-	.overview-list {
-		display: grid;
-		grid-gap: 0.75em;
-		grid-template-columns: repeat(3, 1fr);
-	}
-
-	@media (min-width: 1200px){
-		.overview-list {
-			grid-template-columns: repeat(3, 1fr);
-		}
-	}
-
-	@media (max-width: 1200px) {
-		.overview-list {
-			grid-template-columns: repeat(1, 1fr);
-		}
-	}
-</style>
+<Masonry 
+	{items}
+	{minColWidth}
+	{maxColWidth}
+	{gap}
+	let:item
+	bind:width
+	bind:height
+>
+<Article {item} />
+</Masonry>
