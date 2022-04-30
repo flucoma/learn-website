dcc7e5324db88367ea510d262c2d423abf6e646b
James Bradbury <github@jamesbradbury.net>
2022-04-30 17:31:13 +0800

    components for the reference page

---
+++ src/lib/components/mosaic/RefCategory.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let item = {};
+    const title = item[0];
+    const ref = item[1];
+</script>
+
+<h3>{title}</h3>
+<ul>
+    {#each ref as r}
+    <li><a href={r.url}>{r.title}</a></li>
+    {/each}
+</ul>
+
+<style>
+    h3 {
+        margin-top: 0.25em;
+		margin-bottom: 0.25em;
+    }
+
+    ul {
+        margin: 0;
+		padding: 0;
+    }
+
+    li {
+        display: block;
+		margin: 0;
+    }
+
+    a {
+        text-decoration: none;
+		display: grid;
+		grid-template-columns: auto auto;
+		place-items: center;
+		gap: 0.125em;
+    }
+</style>
---
+++ src/lib/components/mosaic/References.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Masonry from 'svelte-bricks';
+	import RefCategory from '$lib/components/mosaic/RefCategory.svelte'
+	
+	export let items = [];
+
+	let [minColWidth, maxColWidth, gap] = [100, 400, 50]
+	let width, height;
+</script>
+
+<Masonry 
+	{items}
+	{minColWidth}
+	{maxColWidth}
+	{gap}
+	let:item
+	bind:width
+	bind:height
+>
+<RefCategory {item} />
+</Masonry>
+
