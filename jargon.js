09873fc3c817c04ef2ca71e975305d33bed136ec
Ted Moore <ted@tedmooremusic.com>
2022-05-04 16:40:21 +0100

    MLPs

--- jargon.js
+++ jargon.js
@@ -3,7 +3,10 @@
 // WARNING
 
 const definitions = {
-    spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>."
+    spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>.",
+    epoch: "An epoch is when a neural network trains on all of the provided input-output example pairs one time.",
+    epochs: "An epoch is when a neural network trains on all of the provided input-output example pairs one time.",
+    converge: "Converging refers to when a machine learning algorithm (such as a neural network) settles into a state where it is no longer learning from the data provided, hopefully because it has successfully learned all it can from the data. At convergance, one might test the neural network to see how it is performing."
 }
 
 
