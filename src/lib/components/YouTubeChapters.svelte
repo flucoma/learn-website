6e6738c523ba46238d3c806c4c3f1f9644a6b9a8
Jacob <jacob.dchart@gmail.com>
2022-05-05 14:23:12 +0200

    start youtube chapters component

---
+++ src/lib/components/YouTubeChapters.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export let url: string = '';
+	export let title: string = '';
+	export let caption: string = title;
+	export let width: number = 560;
+	export let height: number = 315;
+</script>
+
+<div class="container">
+	<iframe
+		title={title}
+		width={width}
+		height={height}
+		src={`https://www.youtube.com/embed/${url}`}
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+	/>
+	<div class="caption">
+		<p>{caption}</p>
+	</div>
+</div>
+
+<style>
+	.container {
+		padding-top: 1em;
+		padding-bottom: 1em;
+	}
+
+	iframe {
+		max-width: 100%;
+		margin: 0 auto;
+		display: block;
+	}
+
+	.caption :global(p) {
+		font-style: italic;
+		text-align: center;
+		width: 80%;
+		margin: 0 auto;
+		font-size: 0.8rem;
+	}
+</style>
