876b7454546757794d5a96e317d7e4efd3840594
Ted Moore <ted@tedmooremusic.com>
2022-05-05 13:05:10 +0100

    ⚠️

--- jargon.js
+++ jargon.js
@@ -6,7 +6,10 @@ const definitions = {
     spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>.",
     epoch: "An epoch is when a neural network trains on all of the provided input-output example pairs one time.",
     epochs: "An epoch is when a neural network trains on all of the provided input-output example pairs one time.",
-    converge: "Converging refers to when a machine learning algorithm (such as a neural network) settles into a state where it is no longer learning from the data provided, hopefully because it has successfully learned all it can from the data. At convergance, one might test the neural network to see how it is performing."
+    converge: "Converging refers to when a machine learning algorithm (such as a neural network) settles into a state where it is no longer learning from the data provided, hopefully because it has successfully learned all it can from the data. At convergance, one might test the neural network to see how it is performing.",
+    convergence: "Converging refers to when a machine learning algorithm (such as a neural network) settles into a state where it is no longer learning from the data provided, hopefully because it has successfully learned all it can from the data. At convergance, one might test the neural network to see how it is performing.",
+    generalise: "A machine learning model's (such as a neural network) ability to generalise refers to its ability to perform well on data it has never seen before. A model that can generalise is not overfit or underfit.",
+    model: "A machine learning model is an algorithm (such as a neural network) that has been trained on data and is able to make predictions."
 }
 
 
