43958a975592c47f27f3beb92a8685c0d8f10ec4
Ted Moore <ted@tedmooremusic.com>
2022-05-06 16:38:09 +0100

    MLPs ⚠️

--- jargon.js
+++ jargon.js
@@ -9,7 +9,10 @@ const definitions = {
     converge: "Converging refers to when a machine learning algorithm (such as a neural network) settles into a state where it is no longer learning from the data provided, hopefully because it has successfully learned all it can from the data. At convergance, one might test the neural network to see how it is performing.",
     convergence: "Converging refers to when a machine learning algorithm (such as a neural network) settles into a state where it is no longer learning from the data provided, hopefully because it has successfully learned all it can from the data. At convergance, one might test the neural network to see how it is performing.",
     generalise: "A machine learning model's (such as a neural network) ability to generalise refers to its ability to perform well on data it has never seen before. A model that can generalise is not overfit or underfit.",
-    model: "A machine learning model is an algorithm (such as a neural network) that has been trained on data and is able to make predictions."
+    model: "A machine learning model is an algorithm (such as a neural network) that has been trained on data and is able to make predictions.",
+    "supervised learning": "A type of machine learning when an algorithm learns from labeled data, such as data consisting of input-output example pairs.",
+    classification: "The task of identifying what category, or 'class' an input belongs to.",
+    class: "When used in terms of machine learning classification, 'class' is synonymous with 'category'."
 }
 
 
