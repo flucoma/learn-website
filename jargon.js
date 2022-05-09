2e56c8d5dd12de420eb456981f782ac070abdb24
Ted Moore <ted@tedmooremusic.com>
2022-05-09 19:38:57 +0100

    a whack at it ⚠️

--- jargon.js
+++ jargon.js
@@ -3,7 +3,8 @@
 // WARNING
 
 const definitions = {
-    spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>."
+    spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>.",
+    "sampling rate": "The frequency at which an analog-to-digital converter samples voltages to create a digital audio signal. Common sampling rates are 44,100 and 48,000 Hz."
 }
 
 
