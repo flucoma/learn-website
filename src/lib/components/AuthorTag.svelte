1985dd08c9a2856268e5e6fb4555359ef8e84963
James Bradbury <github@jamesbradbury.net>
2022-05-10 10:37:35 +0100

    [Enhance] Learn article authorship (#308)
    * remove authors.js
    
    * remove authors export from authors.js
    
    * update AuthorsTag to use a map of const values
    
    * add authorship to learn articles

--- src/lib/components/AuthorTag.svelte
+++ src/lib/components/AuthorTag.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
 	export let author: string = '';
-	import { authors } from '$lib/app.js';
 
-	const author_name = authors[author]['name'];
-	const author_website = authors[author]['website'];
+	const authors = new Map(
+		Object.entries({
+			Jacob: {
+				name: 'Jacob Hart',
+				website: 'https://jacob-hart.com/'
+			},
+			James: {
+				name: 'James Bradbury',
+				website: 'https://www.jamesbradbury.net/'
+			},
+			Ted: {
+				name: 'Ted Moore',
+				website: 'https://www.tedmooremusic.com/'
+			},
+			PA: {
+				name: 'Pierre Alexandre Tremblay',
+				website: 'https://www.pierrealexandretremblay.com/'
+			},
+			Owen: {
+				name: 'Owen Green',
+				website: 'http://owengreen.net/'
+			}
+		})
+	);
+	const author_name = authors.get(author)['name'];
+	const author_website = authors.get(author)['website'];
 </script>
 
 <p class="author_link">by <a href={author_website}>{author_name}</a></p>
