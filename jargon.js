473155849652e4f3ca03240bf081e60bd62177be
Ted Moore <ted@tedmooremusic.com>
2022-05-10 21:38:03 +0100

    wip

--- jargon.js
+++ jargon.js
@@ -4,7 +4,8 @@
 
 const definitions = {
     spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>.",
-    "sampling rate": "The frequency at which an analog-to-digital converter samples voltages to create a digital audio signal. Common sampling rates are 44,100 and 48,000 Hz."
+    "sampling rate": "The frequency at which an analog-to-digital converter samples voltages to create a digital audio signal. Common sampling rates are 44,100 and 48,000 Hz.",
+    "Nyquist Frequency": "The highest frequency a digital signal can encode, which is half of the sampling rate. The sampling rate is the frequency at which an analog-to-digital converter samples voltages to create a digital audio signal. Common sampling rates are 44,100 and 48,000 Hz."
 }
 
 
