e9bb2872eee86a5e73ce9fe5a986497d4b83a03c
Ted Moore <ted@tedmooremusic.com>
2022-05-11 16:41:29 +0100

    added a comma

--- jargon.js
+++ jargon.js
@@ -8,7 +8,7 @@ const definitions = {
     mean: "The average value of the data. This is calculated by adding up all the numbers and the dividing by how many numbers there are.",
     median: "The value at or below which 50% of the values in the data will fall, also called the 50th percentile.",
     mode: "The value that occurs most frequently in a set of data.",
-	"Nyquist Frequency": "The highest frequency a digital signal can encode, which is half of the sampling rate. The sampling rate is the frequency at which an analog-to-digital converter samples voltages to create a digital audio signal. Common sampling rates are 44,100 and 48,000 Hz."
+	"Nyquist Frequency": "The highest frequency a digital signal can encode, which is half of the sampling rate. The sampling rate is the frequency at which an analog-to-digital converter samples voltages to create a digital audio signal. Common sampling rates are 44,100 and 48,000 Hz.",
     glissandi: "A continuous slide upwards or downwards between two notes."
 }
 
