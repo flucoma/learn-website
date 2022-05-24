a24143228dacd769f373809b1f47843196b181ee
Ted Moore <ted@tedmooremusic.com>
2022-05-24 14:36:20 +0100

    ⚠️

--- jargon.js
+++ jargon.js
@@ -4,6 +4,8 @@
 
 const definitions = {
     "interquartile range": "The range between the 25th and 75th <a href='https://learn.flucoma.org/reference/bufstats'>percentiles</a>. 50% of the data will fall within this range.",
+    quartile:"One quarter of the values in a dimension of data. Usually between evenly divided percentiles: 0th to 25th, 25th to 50th, 50th to 75th, or 75th to 100th percentiles.",
+    quartiles:"One quarter of the values in a dimension of data. Usually between evenly divided percentiles: 0th to 25th, 25th to 50th, 50th to 75th, or 75th to 100th percentiles.",
     spectrum: "See the <a href='https://en.wikipedia.org/wiki/Spectrogram'>wikipedia article</a>.",
     mean: "The average value of the data. This is calculated by adding up all the numbers and the dividing by how many numbers there are.",
     median: "The value at or below which 50% of the values in the data will fall, also called the 50th percentile.",
