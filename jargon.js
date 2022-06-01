3ad3bc57c5b053319f6a644ed2326931aa7a0ac8
Ted Moore <ted@tedmooremusic.com>
2022-06-01 12:06:06 +0100

    linear vs logarithmic scaling

--- jargon.js
+++ jargon.js
@@ -9,7 +9,8 @@ const definitions = {
     median: "The value at or below which 50% of the values in the data will fall, also called the 50th percentile.",
     mode: "The value that occurs most frequently in a set of data.",
     glissandi: "A continuous slide upwards or downwards between two notes.",
-    "euclidean distance":"Using the Pythagorean Theorem (but often in more than two dimensions) to compute the distance between any two points in a multi-dimensional space."
+    "euclidean distance":"Using the Pythagorean Theorem (but often in more than two dimensions) to compute the distance between any two points in a multi-dimensional space.",
+    logarithmic: "A logarithm is the opposite of an exponent. With exponentiation (y = x^2), as 'x' gets bigger, 'y' keeps getting 'exponetially bigger'. With a logarithm (y = log(x)), as x gets bigger, y continues to grow but at a slower and slower rate. See the <a href='https://en.wikipedia.org/wiki/Logarithm'>Wikipedia article</a> for more. <p><a href='https://commons.wikimedia.org/wiki/File:Logarithm_plots.png#/media/File:Logarithm_plots.png'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Logarithm_plots.png/1200px-Logarithm_plots.png' alt='Logarithm plots' width='300px'></a><br>By Richard F. Lyon<a href='https://creativecommons.org/licenses/by-sa/3.0' title='Creative Commons Attribution-Share Alike 3.0'>CC BY-SA 3.0</a>, <a href='https://commons.wikimedia.org/w/index.php?curid=13257335'>Link</a></p>"
 }
 
 export default { definitions };
