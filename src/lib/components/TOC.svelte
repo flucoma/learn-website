98848b49c4eabc185d96d7e90324c8e52b6b13d8
James Bradbury <github@jamesbradbury.net>
2022-06-17 00:18:36 +0100

    mobile friendly navigation

--- src/lib/components/TOC.svelte
+++ src/lib/components/TOC.svelte
@@ -1,6 +1,10 @@
 <script>
 	import { page } from '$app/stores';
+	import { slide } from 'svelte/transition'
 	import { structure } from '$lib/app.js';
+	let w;
+	let expand = false;
+	const breakpoint = 600;
 
 	let headings = [];
 	$: {
@@ -10,22 +14,78 @@
 	}
 </script>
 
-<nav class="container">
+{#if w >= breakpoint}
+<div class="container">
 	<div>
 		{#if headings.length > 0}
-			<h3 class="toc">Table of Contents</h3>
-			<div class="headings">
-				{#each headings as h}
-					<a href={h.url}>{h.text}</a>
-				{/each}
-			</div>
+		<h3 class="toc">Table of Contents</h3>
+		<div class="headings">
+			{#each headings as h}
+				<a href={h.url}>{h.text}</a>
+			{/each}
+		</div>
 		{/if}
 	</div>
-</nav>
+</div>
+{/if}
+{#if w < breakpoint}
+<div class="mobile-toc">
+	<button on:click={()=>{expand=!expand}}>On This Page</button>
+	<div class="line"></div>
+	{#if headings.length > 0 && expand}
+	<div class="onthispage" transition:slide={{duration:200}}>
+		{#each headings as h}
+			<a href={h.url}>{h.text}</a>
+		{/each}
+	</div>
+	{/if}
+</div>
+{/if}
 
-<svelte:window />
+<svelte:window bind:innerWidth={w}/>
 
 <style lang="postcss">
+	.mobile-toc {
+		border-radius: 10px;
+		background-color: var(--med-blue);
+		width: 100%;
+	}
+
+	.mobile-toc > button {
+		font-family: var(--font);
+		width: 100%;
+		border: 0;
+		background: none;
+		box-shadow: none;
+		border-radius: 0px;
+		color: white;
+	}
+
+	.onthispage {
+		background-color: var(--med-blue);
+		border-radius: 10px;
+		display: flex;
+		flex-direction: column;
+		gap: 0.25em;
+		padding: 0.5em;
+	}
+
+	.line {
+		height: 1px;
+		background-color: white;
+	}
+
+	.onthispage > a {
+		color: white;
+		padding: 6px;
+	}
+
+	.onthispage > a:hover {
+		background-color: var(--dark-blue);
+		border-radius: 10px;
+		
+	}
+
 	.container {
 		display: flex;
 		flex-direction: column;
@@ -35,7 +95,7 @@
 		max-width: 23ch;
 	}
 
-	.headings {
+	.headings, .onthispage {
 		display: flex;
 		flex-direction: column;
 		gap: 0.5em;
