4a755f73c1090bcef41dacd1c85eb7d3c6bb9f1f
James Bradbury <github@jamesbradbury.net>
2022-08-03 15:55:49 +0100

    work on widgets

---
+++ src/lib/widget/time/NoiseShape.svelte
@@ -0,0 +1,28 @@
+<!--
+@component
+A component that plays back filtered noise, while showing how a curve can be drawn to represent its volume
+-->
+
+<script>
+	import { onMount } from 'svelte';
+	import * as Tone from 'tone';
+
+	let src; // a sound source
+	let lfo; // an LFO to modulate the sound source
+	let probe; // extract the value of the lfo
+	let gain;
+	let probeReading = 0; // the value the probe reads
+
+	onMount(async() => {
+	})
+
+	const start = async() => {
+		await Tone.start();
+		src = new Tone.Noise('pink').toDestination().start();
+		lfo = new Tone.LFO(1, 0, 1);
+		probe = new Tone.Meter(0);
+		
+	}
+</script>
+
+<button on:click={start}>start</button>
