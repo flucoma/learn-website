a294664e2bc7a770bb4f6cb5973e1ae43d59ecbe
James Bradbury <github@jamesbradbury.net>
2022-08-09 16:46:33 +0100

    component for linking to static examples

---
+++ src/lib/components/ExploreDownload.svelte
@@ -0,0 +1,14 @@
+<script>
+	import IndentNote from './IndentNote.svelte';
+	import { createLink } from '$lib/assets';
+	export let url = '';
+	export let note = '';
+
+	const link = createLink(url);
+</script>
+
+<IndentNote type="pointer">
+	Download the demonstration patches for this article <a href={link} download='Alice Eldridge and Chris Kiefer Examples.zip'>here</a>.
+	
+	{ @html note }
+</IndentNote>
