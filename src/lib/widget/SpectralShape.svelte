e9bfb1c9d0d1d373e57388b28b3a86f9bfaeb9cf
James Bradbury <github@jamesbradbury.net>
2022-08-12 14:20:34 +0100

    simplify widget

--- src/lib/widget/SpectralShape.svelte
+++ src/lib/widget/SpectralShape.svelte
@@ -1,17 +1,18 @@
 <script>
 	import * as Meyda from 'meyda';
-	import * as Tone from 'tone';
 	import { binToHz } from '$lib/util';
+	import Audio from '$lib/components/Audio.svelte';
 
 	import Button from '$lib/components/Button.svelte';
-	import SourceGenerator from './SourceGenerator.svelte';
 	import Spectrogram from './Spectrogram.svelte';
 
 	const windowSize = 512;
-	const bufferSize = windowSize;
+	const bufferSize = 512;
+
+	let audio;
 
 	// Visuals
-	let chart, audioContext, generator, source;
+	let chart, audioContext;
 	let enabled = false;
 
 	let shape = {
@@ -24,15 +25,15 @@
 		crest: 0
 	};
 
-	function startAudioDescriptor() {
-		if (!enabled) {
-			Tone.start();
-			generator.init();
-			generator.patch('osc'); // activate the sine tone generator
-			enabled = true;
-		}
+	$: console.log(audio)
 
-		// Provide with the Tone.js audio context;
+	function startAudioDescriptor() {
+		enabled = true;
+		const AudioContext = window.AudioContext || window.webkitAudioContext;
+		audioContext = new AudioContext();
+		const source = audioContext.createMediaElementSource(audio);
+		source.connect(audioContext.destination);
+			
 		const analyser = Meyda.createMeydaAnalyzer({
 			audioContext: audioContext,
 			source: source,
@@ -70,29 +71,40 @@
 	$: if (chart) chart.options.plugins.annotation.annotations.rolloff.xMax = Math.min(shape.rolloff, 20000);
 </script>
 
+
 <div class="container">
-	<Button
+	<div class="start">
+		<Button
 		on:click={startAudioDescriptor}
 		label="Start Audio"
-		width="max-content"
-		height="max-content"
+		width="200px"
+		height="40px"
 		disabled={enabled}
-	/>
-
-	<Spectrogram bind:chart />
-
-	<div class="controllers">
-		<SourceGenerator bind:audioContext bind:output={source} bind:this={generator} />
-
-		<div class="values">
-			{#each Object.entries(shape) as [k, v]}
-				<div class="descriptor">
-					<div class:centroid={k === 'centroid'} class:rolloff={k === 'rolloff'}>{k}:</div>
-					<div class:centroid={k === 'centroid'} class:rolloff={k === 'rolloff'}>{v.toFixed(1)}</div>
-				</div>
-			{/each}
-		</div>
+		/>
 	</div>
+	<div class="frame" class:disabled={!enabled}>
+		<div class="plot" >
+			<Spectrogram bind:chart />
+
+			<div class="values">
+				{#each Object.entries(shape) as [k, v]}
+					<div class="descriptor">
+						<div class:centroid={k === 'centroid'} class:rolloff={k === 'rolloff'}>{k}:</div>
+						<div class:centroid={k === 'centroid'} class:rolloff={k === 'rolloff'}>{v.toFixed(1)}</div>
+					</div>
+				{/each}
+			</div>
+		</div>
+
+		<Audio 
+		src='/reference/spectralshape/passage.mp3'
+		bind:audio
+		label='Evolving Sound'
+		waveform={true}
+		loop={true}
+		/>
+	</div>
+
 </div>
 
 <style>
@@ -107,16 +119,23 @@
 		flex-direction: column;
 		margin-top: 1em;
 		margin-bottom: 1em;
-		border: 0.063rem solid #dcdee0;
-		border-radius: 0.25em;
-		padding: 0.5em;
-		gap: 0.25em;
+		gap: 2em;
 	}
 
-	.controllers {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
+	.start {
+		display: grid;
+		place-items: center;
+	}
+
+	.plot {
+		display: grid;
+		grid-template-columns: auto max-content;
+		gap: 0.5em;
+	}
+
+	.disabled {
+		opacity: 0.5;
+		pointer-events: none;
 	}
 
 	.values {
