3677167a7d0c7955510fc327c9d82121bdcab215
James Bradbury <github@jamesbradbury.net>
2022-08-21 13:30:54 +0100

    getting started work

---
+++ src/routes/learn/gs.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Map from '$$lib/components/getting-started/Test.sveltestarted/Map.svelte';
+</script>
+
+<div class="grid">
+	<Map />
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(2, auto);
+		gap: 1em;
+		width: 100%;
+		align-content: center;
+		justify-content: space-around;
+	}
+
+
+</style>
