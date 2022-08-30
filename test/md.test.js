2f3d08b3c72834409fd9bdc864c392902ef667ae
James Bradbury <github@jamesbradbury.net>
2022-08-30 19:59:46 +0100

    add test for markdown structure

---
+++ test/md.test.js
@@ -0,0 +1,61 @@
+const fg = require('fast-glob')
+const frontmatter = require('front-matter');
+const { markdown } = require('markdown');
+const fs = require('fs');
+
+const categories = ['Decompose', 'Analyse Sound', 'Analyse Data', 'Utility', 'Transform', 'Slicing'];
+const ignores = [
+	'src/routes/reference/+page.svx',
+	'src/routes/explore/+page.svx',
+	'src/routes/learn/+page.svx',
+	'src/routes/+page.svx',
+	'src/routes/installation/+page.svx'
+];
+const routes = fg.sync(['src/routes/(learn|reference|explore)/**/*+page.svx'])
+.filter(route => !ignores.includes(route));
+
+routes.forEach(route => {
+	const section = route.split('/')[2];
+ 	const data = fs.readFileSync(route, 'utf8');
+
+	// Get frontmatter
+	const fm = frontmatter(data).attributes;
+
+	test('frontmatter has necessary structure', () => {
+		expect(fm).toHaveProperty('title');
+		expect(fm).toHaveProperty('blurb');
+		expect(fm).toHaveProperty('tags');
+		expect(fm).toHaveProperty('flair');
+	})
+	test('title is valid', () => {
+		expect(typeof fm.title).toBe('string')
+	})
+	test('array of tags supplied', () => {
+		expect(typeof fm.tags).toBe('object')
+		expect(fm.tags.length).toBeGreaterThan(0);
+	})
+	test('flair is valid', () => {
+		const flair = fm.flair;
+		expect(flair).toMatch(new RegExp('reference|article|podcast|event|tutorial'))
+	})
+
+	if (section === 'reference') {
+		test('reference frontmatter', () => {
+			expect(fm).toHaveProperty('category');
+			expect(categories).toContain(fm.category);
+		})
+	}
+
+	console.log(fm)
+	const tree = markdown.parse(data);
+
+	const headers = tree.filter(x => x.includes('header'));
+	const h1 = headers.filter(x => x[1].level === 1)
+
+	test(`No <h1/> (#) defined in markdown ${route}`, () => {
+		expect(h1.length).toBe(0);
+	})
+})
+
+
+
