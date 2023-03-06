1b094c088c677dd532f4383ee8aa7957532b232c
James Bradbury <github@jamesbradbury.net>
2023-03-06 19:37:56 +0100

    add white option for arrows

--- src/lib/components/ArrowRight.svelte
+++ src/lib/components/ArrowRight.svelte
@@ -1,5 +1,9 @@
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"
-	><path d="M0 0h24v24H0z" fill="none" /><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" /></svg
+<script>
+	export let white = false;
+</script>
+
+<svg class:white={white} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"
+	><path  d="M0 0h24v24H0z" fill="none" /><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" /></svg
 >
 
 <style>
@@ -7,5 +11,9 @@
 		fill: var(--med-blue);
 		width: 16px;
 		height: 16px;
+	}
+
+	.white {
+		fill: white;
 	}
 </style>
