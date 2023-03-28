8fc48001198db2780b484d03bbe3d1495d27b4db
James Bradbury <github@jamesbradbury.net>
2023-03-28 09:41:03 +0800

    add podcast page

---
+++ src/routes/(supplementary)/podcast/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../../../app.css'
+</script>
+
+<main>
+	<slot></slot>
+</main>
+
+<style>
+	main {
+		padding: 1em;
+		background: linear-gradient(90deg, rgba(255,255,255,0.25) 55%, rgba(146,200,246,0.25) 82%, rgba(71,158,248,0.25) 100%);
+	}
+</style>
