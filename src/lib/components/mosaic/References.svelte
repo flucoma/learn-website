386ccfa0aca6c7dbf2f4eafcfe9b8c2403c45ead
James Bradbury <github@jamesbradbury.net>
2023-08-15 10:32:42 +0200

    fix 421

--- src/lib/components/mosaic/References.svelte
+++ src/lib/components/mosaic/References.svelte
@@ -1,13 +1,20 @@
 <script>
-	import Masonry from 'svelte-bricks';
 	import RefCategory from '$lib/components/mosaic/RefCategory.svelte';
 
 	export let items = [];
-
-	let [minColWidth, maxColWidth, gap] = [150, 250, 25];
-	let width, height;
 </script>
 
-<Masonry {items} {minColWidth} {maxColWidth} {gap} idKey={'category'} animate={true} let:item bind:width bind:height>
-	<RefCategory {item} />
-</Masonry>
+<div class="items">
+	{#each items as item}
+		<RefCategory {item} />
+	{/each}
+</div>
+
+<style>
+	.items {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 1em;
+	}
+</style>
